6a098a222b24da02d3fb35b8246848c9b127233a
Sean Cross <sean@xobs.io>
2020-08-07 18:58:36 +0800

    xous: Add missing `mem` functions
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ xous-rs/src/arch/native/mem.rs
@@ -0,0 +1,32 @@
+use crate::{Error, MemoryAddress, MemoryFlags, MemoryRange};
+
+pub fn map_memory_pre(
+    _phys: &Option<MemoryAddress>,
+    _virt: &Option<MemoryAddress>,
+    _size: usize,
+    _flags: MemoryFlags,
+) -> core::result::Result<(), Error> {
+    Ok(())
+}
+
+pub fn map_memory_post(
+    _phys: Option<MemoryAddress>,
+    _virt: Option<MemoryAddress>,
+    _size: usize,
+    _flags: MemoryFlags,
+    range: MemoryRange,
+) -> core::result::Result<MemoryRange, Error> {
+    Ok(range)
+}
+
+pub fn unmap_memory_pre(
+    _range: &MemoryRange
+) -> core::result::Result<(), Error> {
+    Ok(())
+}
+
+pub fn unmap_memory_post(
+    _range: MemoryRange
+) -> core::result::Result<(), Error> {
+    Ok(())
+}
