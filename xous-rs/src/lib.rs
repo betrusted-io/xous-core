3ed964507df7a5bc256e68fae93affd5bc4b24bb
Sean Cross <sean@xobs.io>
2020-08-14 16:44:08 +0800

    xous: add #[xous::xous_main] proc-macro
    This is used to annotate a main function that is compatible with both
    hosted and baremetal environments.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- xous-rs/src/lib.rs
+++ xous-rs/src/lib.rs
@@ -3,6 +3,10 @@
 #[macro_use]
 extern crate bitflags;
 
+extern crate xous_macros as macros;
+
+pub use macros::xous_main;
+
 pub mod arch;
 
 pub mod carton;
@@ -26,4 +30,42 @@ macro_rules! make_name {
         c.copy_from_slice($fcc.as_bytes());
         u32::from_le_bytes(c) as usize
     }};
+}
+
+#[cfg(not(target_os = "none"))]
+#[macro_export]
+macro_rules! maybe_main {
+    () => {
+        extern "Rust" {
+            fn xous_entry() -> !;
+        }
+        fn main() {
+            unsafe { xous_entry() };
+        }
+    };
+}
+
+#[cfg(target_os = "none")]
+#[macro_export]
+macro_rules! maybe_main {
+    () => {
+        use core::panic::PanicInfo;
+
+        #[panic_handler]
+        fn handle_panic(_arg: &PanicInfo) -> ! {
+            // println!("PANIC!");
+            // println!("Details: {:?}", arg);
+            xous::syscall::wait_event();
+            loop {}
+        }
+
+        extern "Rust" {
+            fn xous_entry() -> !;
+        }
+
+        #[export_name = "_start"]
+        pub extern "C" fn _start() {
+            unsafe { xous_entry() };
+        }
+    };
 }
