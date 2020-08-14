6abe2af34e738262b96cec844878a6cad6a3c3cf
Sean Cross <sean@xobs.io>
2020-08-14 18:20:37 +0800

    graphics: use dumb lfsr instead of rand crate
    Signed-off-by: Sean Cross <sean@xobs.io>

--- examples/graphics/src/backend/minifb.rs
+++ examples/graphics/src/backend/minifb.rs
@@ -2,11 +2,9 @@ use minifb::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 336;
 const HEIGHT: usize = 536;
-const MAX_FPS: u64 = 10;
+const MAX_FPS: u64 = 15;
 const LIGHT_COLOUR: u32 = 0xB5B5AD;
 const DARK_COLOUR: u32 = 0x1B1B19;
-
-use rand::prelude::*;
 
 pub fn run() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -30,10 +28,13 @@ pub fn run() {
         1000 * 1000 / MAX_FPS,
     )));
 
-    let mut rng = rand::thread_rng();
+    let mut lfsr = 0xace1u32;
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for i in buffer.iter_mut() {
-            *i = if rng.gen() { LIGHT_COLOUR } else { DARK_COLOUR };
+            lfsr ^= lfsr >> 7;
+            lfsr ^= lfsr << 9;
+            lfsr ^= lfsr >> 13;
+            *i = if lfsr & 1 == 0 { LIGHT_COLOUR } else { DARK_COLOUR };
         }
 
         // We unwrap here as we want this code to exit if it fails.
