82c9915ad6ea1502a8befd9b379646ca82b8a2e1
Sean Cross <sean@xobs.io>
2020-08-17 19:41:50 +0800

    examples: append `-server` to server names
    This keeps server names such as "log" and "graphics" as unambiguous.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- examples/graphics-server/src/backend/mod.rs
+++ examples/graphics-server/src/backend/mod.rs
@@ -2,3 +2,8 @@
 mod minifb;
 #[cfg(not(target_os = "none"))]
 pub use crate::backend::minifb::*;
+
+#[cfg(target_os = "none")]
+mod betrusted;
+#[cfg(target_os = "none")]
+pub use crate::backend::betrusted::*;
