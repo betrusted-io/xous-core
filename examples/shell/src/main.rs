7d8f5898309033ba2998d74d35f1da3b8c7276ee
Sean Cross <sean@xobs.io>
2020-08-29 22:09:20 +0800

    examples: get three-process IPC working
    The log, graphics, and shell processes now all talk to one another. This
    illustrates how multiple processes can communicate regardless of what is
    going on underneath.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- examples/shell/src/main.rs
+++ examples/shell/src/main.rs
@@ -16,50 +16,137 @@ mod timer;
 //     }
 // }
 
+fn move_lfsr(mut lfsr: u32) -> u32 {
+    lfsr ^= lfsr >> 7;
+    lfsr ^= lfsr << 9;
+    lfsr ^= lfsr >> 13;
+    lfsr
+}
+
+fn ensure_connection(server: xous::SID) -> xous::CID {
+    loop {
+        if let Ok(cid) = xous::syscall::connect(server) {
+            return cid;
+        }
+        xous::syscall::yield_slice();
+    }
+}
+
 #[xous::xous_main]
 fn shell_main() -> ! {
     timer::init();
 
-    let mut connection = None;
+    let log_server_id = xous::SID::from_bytes(b"xous-logs-output").unwrap();
+    let graphics_server_id = xous::SID::from_bytes(b"graphics-server ").unwrap();
 
-    let sid = xous::SID::from_bytes(b"xous-logs-output").unwrap();
+    println!("SHELL: Attempting to connect to servers...");
+    let log_conn = ensure_connection(log_server_id);
+    let graphics_conn = ensure_connection(graphics_server_id);
 
-    println!("Attempting to connect to server...");
-    while connection.is_none() {
-        if let Ok(cid) = xous::syscall::connect(sid) {
-            connection = Some(cid);
-        } else {
-            xous::syscall::yield_slice();
-        }
-    }
-    let connection = connection.unwrap();
-    println!("Connected: {:?}", connection);
+    println!(
+        "SHELL: Connected to Log server: {}  Graphics server: {}",
+        log_conn, graphics_conn
+    );
+
+    assert_ne!(
+        log_conn, graphics_conn,
+        "SHELL: graphics and log connections are the same!"
+    );
 
     let mut counter: usize = 0;
     // let ls = logstr::LogStr::new();
+    let mut lfsr = 0xace1u32;
+    let dark = graphics_server::Color::from(0);
+    let light = graphics_server::Color::from(!0);
     loop {
-        println!("Sending a scalar message with id {}...", counter + 4096);
-        match xous::syscall::send_message(
-            connection,
-            xous::Message::Scalar(xous::ScalarMessage {
-                id: counter + 4096,
-                arg1: counter,
-                arg2: counter * 2,
-                arg3: !counter,
-                arg4: counter + 1,
-            }),
-        ) {
-            Err(xous::Error::ServerQueueFull) => {
-                println!("Server queue is full... retrying");
-                continue;
-            },
-            Ok(_) => {
-                println!("Loop {}", counter);
-                counter += 1;
-            },
-            Err(e) => panic!("Unable to send message: {:?}", e),
+        // println!("Sending a scalar message with id {}...", counter + 4096);
+        // match xous::syscall::send_message(
+        //     log_conn,
+        //     xous::Message::Scalar(xous::ScalarMessage {
+        //         id: counter + 4096,
+        //         arg1: counter,
+        //         arg2: counter * 2,
+        //         arg3: !counter,
+        //         arg4: counter + 1,
+        //     }),
+        // ) {
+        //     Err(xous::Error::ServerQueueFull) => {
+        //         // println!("Server queue is full... retrying");
+        //         continue;
+        //     }
+        //     Ok(_) => {
+        //         println!("Loop {}", counter);
+        //         counter += 1;
+        //     }
+        //     Err(e) => panic!("Unable to send message: {:?}", e),
+        // }
+
+        // lfsr = move_lfsr(lfsr);
+
+        loop {
+            match graphics_server::set_style(
+                graphics_conn,
+                5,
+                if lfsr & 1 == 0 { dark } else { light },
+                if lfsr & 1 == 0 { dark } else { light },
+            ) {
+                Err(xous::Error::ServerQueueFull) => continue,
+                Ok(_) => break,
+                Err(e) => panic!("unable to draw to screen: {:?}", e),
+            }
+        }
+        let x1 = move_lfsr(lfsr);
+        let y1 = move_lfsr(x1);
+        let x2 = move_lfsr(y1);
+        let y2 = move_lfsr(x2);
+        lfsr = y2;
+
+        loop {
+            match graphics_server::draw_line(
+                graphics_conn,
+                graphics_server::Point::new((x1 % 336) as _, (y1 % 536) as _),
+                graphics_server::Point::new((x2 % 336) as _, (y2 % 536) as _),
+            ) {
+                Err(xous::Error::ServerQueueFull) => continue,
+                Ok(_) => break,
+                Err(e) => panic!("unable to draw to screen: {:?}", e),
+            }
         }
 
+        loop {
+            match graphics_server::flush(graphics_conn) {
+                Err(xous::Error::ServerQueueFull) => continue,
+                Ok(_) => break,
+                Err(e) => panic!("unable to draw to screen: {:?}", e),
+            }
+        }
+
+        let lfsr = move_lfsr(lfsr);
+        if lfsr.trailing_zeros() >= 3 {
+            loop {
+                match xous::syscall::send_message(
+                    log_conn,
+                    xous::Message::Scalar(xous::ScalarMessage {
+                        id: counter + 4096,
+                        arg1: counter,
+                        arg2: counter * 2,
+                        arg3: !counter,
+                        arg4: lfsr as _,
+                    }),
+                ) {
+                    Err(xous::Error::ServerQueueFull) => {
+                        println!("SHELL: Log Server queue is full... retrying");
+                        continue;
+                    }
+                    Ok(_) => {
+                        println!("SHELL: Loop {}", counter);
+                        counter += 1;
+                        break;
+                    }
+                    Err(e) => panic!("Unable to send message: {:?}", e),
+                }
+            }
+        }
         // #[cfg(not(target_os = "none"))]
         // std::thread::sleep(std::time::Duration::from_millis(500));
         // if counter & 2 == 0 {
