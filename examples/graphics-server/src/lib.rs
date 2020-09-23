4a8590d33621b954da52741cf60ff9a45b69ff83
Sean Cross <sean@xobs.io>
2020-09-23 10:58:09 +0800

    graphics-server: fix to change in send_message syntax
    Signed-off-by: Sean Cross <sean@xobs.io>

--- examples/graphics-server/src/lib.rs
+++ examples/graphics-server/src/lib.rs
@@ -6,21 +6,21 @@ pub use api::{Point, Color};
 use xous::{try_send_message, CID};
 
 pub fn draw_line(cid: CID, start: Point, end: Point) -> Result<(), xous::Error> {
-    try_send_message(cid, api::Opcode::Line(start, end).into())
+    try_send_message(cid, api::Opcode::Line(start, end).into()).map(|_| ())
 }
 
 pub fn draw_circle(cid: CID, center: Point, radius: u32) -> Result<(), xous::Error> {
-    try_send_message(cid, api::Opcode::Circle(center, radius).into())
+    try_send_message(cid, api::Opcode::Circle(center, radius).into()).map(|_| ())
 }
 
 pub fn draw_rectangle(cid: CID, start: Point, end: Point) -> Result<(), xous::Error> {
-    try_send_message(cid, api::Opcode::Rectangle(start, end).into())
+    try_send_message(cid, api::Opcode::Rectangle(start, end).into()).map(|_| ())
 }
 
 pub fn set_style(cid: CID, width: u32, stroke: Color, fill: Color) -> Result<(), xous::Error> {
-    try_send_message(cid, api::Opcode::Style(width, stroke, fill).into())
+    try_send_message(cid, api::Opcode::Style(width, stroke, fill).into()).map(|_| ())
 }
 
 pub fn flush(cid: CID) -> Result<(), xous::Error> {
-    try_send_message(cid, api::Opcode::Flush.into())
+    try_send_message(cid, api::Opcode::Flush.into()).map(|_| ())
 }
