bcbfec466e15ae7e1ee1d8522a63523f0b8673e9
Sean Cross <sean@xobs.io>
2020-10-05 10:40:29 +0800

    utra: add real package in
    This does not use a submodule.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ utra/build.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    let svd_file = env::var("XOUS_SVD_FILE").unwrap_or("../example.svd".to_owned());
+    println!("cargo:rerun-if-env-changed=XOUS_SVD_FILE");
+    println!("cargo:rerun-if-env-changed={}", svd_file);
+
+    let src_file = std::fs::File::open(svd_file).expect("couldn't open src file");
+    let mut dest_file = std::fs::File::create("src/generated.rs").expect("couldn't open dest file");
+    svd2utra::generate(src_file, &mut dest_file).unwrap();
+}
