4847469f039b70d1d1adcfbaa088b3f2d4462b8c
Sean Cross <sean@xobs.io>
2020-10-11 09:08:30 +0800

    svd2utra: implement `svd2utra` command
    This now has proper command line handling.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- svd2utra/src/main.rs
+++ svd2utra/src/main.rs
@@ -1,12 +1,44 @@
 mod generate;
-use generate::*;
 
+use anyhow::Context;
+use clap::{App, Arg};
 use std::fs::File;
+use std::io::{Read, Write};
 
-fn main() {
-    println!("Generating using examples/soc.svd, output is example.rs (this stub is for testing only!)");
-    println!("Use `cargo xtask ci <path-to-svdfile>` to generate a final SVD file.");
-    let src = File::open("examples/soc.svd").unwrap();
-    let mut dest = File::create("example.rs").unwrap();
-    generate(src, &mut dest).unwrap();
+fn main() -> anyhow::Result<()> {
+    let matches = App::new("svd2utra")
+        .about("Generate a UTRA Rust API from SVD files")
+        .arg(
+            Arg::with_name("input")
+                .help("Input SVD file")
+                .short("i")
+                .takes_value(true)
+                .value_name("FILE"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .help("Output .rs file or crate directory")
+                .short("o")
+                .takes_value(true)
+                .value_name("FILE"),
+        )
+        .version(concat!(
+            env!("CARGO_PKG_VERSION"),
+            include_str!(concat!(env!("OUT_DIR"), "/commit-info.txt"))
+        ))
+        .get_matches();
+
+    let src: Box<dyn Read> = match matches.value_of("input") {
+        Some(file) => Box::new(File::open(file).context("Cannot open the SVD file")?),
+        None => Box::new(std::io::stdin()),
+    };
+
+    let mut dest: Box<dyn Write> = match matches.value_of("output") {
+        None => Box::new(std::io::stdout()),
+        Some(path) => Box::new(File::open(path).context("Cannot open destination file")?),
+    };
+
+    generate::generate(src, &mut dest).context("Cannot generate output file")?;
+
+    Ok(())
 }
