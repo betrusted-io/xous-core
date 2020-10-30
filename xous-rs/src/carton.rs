a35a75b1f2aa092c35f90d04dd4f8e13400d53b8
Sean Cross <sean@xobs.io>
2020-10-30 10:11:35 +0800

    xous: carton: use `send_message()`
    This function now works as expected.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- xous-rs/src/carton.rs
+++ xous-rs/src/carton.rs
@@ -62,7 +62,7 @@ impl<'a> Carton<'a> {
             offset: None,
             valid: None,
         };
-        crate::try_send_message(connection, Message::Borrow(msg))
+        crate::send_message(connection, Message::Borrow(msg))
     }
 
     /// Perform a mutable lend of this Carton to the server.
@@ -73,7 +73,7 @@ impl<'a> Carton<'a> {
             offset: None,
             valid: None,
         };
-        crate::try_send_message(connection, Message::MutableBorrow(msg))
+        crate::send_message(connection, Message::MutableBorrow(msg))
     }
 }
 
