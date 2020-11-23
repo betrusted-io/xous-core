c36615d0149ba095d99e11b46110eb8f89521491
Sean Cross <sean@xobs.io>
2020-11-23 17:18:44 +0800

    utralib: use canonicalized path for svd in rebuilding
    Use the full path to the .svd file to ensure it is rebuilt when the file
    changes.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- utralib/build.rs
+++ utralib/build.rs
@@ -1,12 +1,14 @@
 use std::env;
 
 fn main() {
-    let svd_file = env::var("XOUS_SVD_FILE")
+    let svd_filename = env::var("XOUS_SVD_FILE")
         .expect("Set the environment variable `XOUS_SVD_FILE` to point to an SVD file");
     println!("cargo:rerun-if-env-changed=XOUS_SVD_FILE");
-    println!("cargo:rerun-if-changed={}", svd_file);
 
-    let src_file = std::fs::File::open(svd_file).expect("couldn't open src file");
+    let svd_file_path = std::path::Path::new(&svd_filename);
+    println!("cargo:rerun-if-changed={}", svd_file_path.canonicalize().unwrap().display());
+
+    let src_file = std::fs::File::open(svd_filename).expect("couldn't open src file");
     let mut dest_file = std::fs::File::create("src/generated.rs").expect("couldn't open dest file");
     svd2utra::generate(src_file, &mut dest_file).unwrap();
 }
