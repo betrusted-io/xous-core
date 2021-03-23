91647592ca548b7cc5a9dbd1ffb00ac595620611
Sean Cross <sean@xobs.io>
2021-03-23 19:53:54 +0800

    kernel-test: implement sleep test
    This patch implements a simple sleep test.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- services/kernel-test/src/main.rs
+++ services/kernel-test/src/main.rs
@@ -1,59 +1,44 @@
 #![cfg_attr(target_os = "none", no_std)]
 #![cfg_attr(target_os = "none", no_main)]
 
-struct LoopArg {
-    msec: usize,
-}
-// fn sleep_10_loop(_ign: usize) {
-//     let ticktimer_server_id = xous::SID::from_bytes(b"ticktimer-server").unwrap();
-//     let ticktimer_conn = xous::connect(ticktimer_server_id).unwrap();
-
-//     let mut ms_count = 1;
-//     loop {
-//         log::info!("10ms Loop count {}", ms_count);
-//         sleep_ms(ticktimer_conn, 10).unwrap();
-//         ms_count += 1;
-//     }
-// }
-
-// fn sleep_5_loop(_ign: usize) {
-//     let ticktimer_server_id = xous::SID::from_bytes(b"ticktimer-server").unwrap();
-//     let ticktimer_conn = xous::connect(ticktimer_server_id).unwrap();
-
-//     let mut ms_count = 1;
-//     loop {
-//         log::info!("5ms Loop count {}", ms_count);
-//         sleep_ms(ticktimer_conn, 5).unwrap();
-//         ms_count += 1;
-//     }
-// }
-
-fn sleep_loop(_arg: usize) {
+fn sleep_loop(sleep_ms: usize) {
     let ticktimer_server_id = xous::SID::from_bytes(b"ticktimer-server").unwrap();
     let ticktimer_conn = xous::connect(ticktimer_server_id).unwrap();
     let tid = xous::current_tid().unwrap();
     let sleep_ms = match tid {
-        2 => 2,
-        3 => 5,
-        4 => 10,
+        4 => 50,
+        2 => 10,
+        3 => 20,
         _ => panic!("Unknown TID"),
     };
-    log::info!("My thread number is {}, sleeping {} ms", tid, sleep_ms);
+    // log::info!("My thread number is {}, sleeping 0x{:08x} ({}) ms", tid, sleep_ms, sleep_ms);
 
-    let mut ms_count = 1;
+    let mut loop_count = 0;
     loop {
-        log::info!("{}ms Loop count {}", sleep_ms, ms_count);
+        // log::info!(
+        //     "TEST THREAD {}: {}ms Number of times slept: {}",
+        //     tid,
+        //     sleep_ms,
+        //     loop_count
+        // );
+        let start_time = ticktimer_server::elapsed_ms(ticktimer_conn).unwrap();
         ticktimer_server::sleep_ms(ticktimer_conn, sleep_ms).unwrap();
-        ms_count += 1;
+        let end_time = ticktimer_server::elapsed_ms(ticktimer_conn).unwrap();
+        log::info!(
+            "TEST THREAD {}: target {}ms, {} loops: Sleep finished (uptime: {}, took {} ms)",
+            tid,
+            sleep_ms,
+            loop_count,
+            end_time,
+            end_time - start_time,
+        );
+        loop_count += 1;
     }
 }
 
 #[xous::xous_main]
 fn test_main() -> ! {
     log_server::init_wait().unwrap();
-    // let ticktimer_server_id = xous::SID::from_bytes(b"ticktimer-server").unwrap();
-    // let ticktimer_conn = xous::connect(ticktimer_server_id).unwrap();
-
     xous::create_thread_simple(sleep_loop, 10).unwrap();
     xous::create_thread_simple(sleep_loop, 5).unwrap();
     xous::create_thread_simple(sleep_loop, 2).unwrap();
