fd255d8f53b55d3595593c03544b844a23f2cbec
Sean Cross <sean@xobs.io>
2021-03-25 23:39:35 +0800

    services: add rkyv-test-client and rkyv-test-server
    These servers demonstrate how we might use rkyv in the future.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ services/rkyv-test-client/src/main.rs
@@ -0,0 +1,27 @@
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg_attr(target_os = "none", no_main)]
+
+fn print_log_messages(log_message: &str, prefix: &str) {
+    log::info!("Got a message hook! Message: [{}], prefix: [{}]", log_message, prefix);
+}
+
+#[xous::xous_main]
+fn rkyv_test_client() -> ! {
+    log_server::init_wait().unwrap();
+    log::info!(
+        "Hello, world! This is the client, PID {}",
+        xous::current_pid().unwrap().get()
+    );
+    let ticktimer_server_id = xous::SID::from_bytes(b"ticktimer-server").unwrap();
+    let ticktimer_conn = xous::connect(ticktimer_server_id).unwrap();
+
+    rkyv_test_server::hook_log_messages(print_log_messages);
+
+    let mut idx = 0;
+    loop {
+        log::info!("2 + {} = {}", idx, rkyv_test_server::add(2, idx).unwrap());
+        ticktimer_server::sleep_ms(ticktimer_conn, 500).ok();
+        rkyv_test_server::log_message("prefix", "Hi there");
+        idx += 1;
+    }
+}
---
+++ services/rkyv-test-server/src/lib.rs
@@ -0,0 +1,158 @@
+#![cfg_attr(target_os = "none", no_std)]
+
+mod api;
+mod buffer;
+
+use core::sync::atomic::{AtomicU32, Ordering};
+use num_traits::{FromPrimitive, ToPrimitive};
+
+// Note that connection IDs are never 0, so this is invalid, and could map to `None`.
+static SERVER_CID: AtomicU32 = AtomicU32::new(0);
+
+/// Ensure we have a connection to our own server. If a connection does not exist,
+/// have the nameserver create one for us.
+fn ensure_connection() -> xous::CID {
+    let mut cid = SERVER_CID.load(Ordering::Relaxed);
+    if cid == 0 {
+        cid = xous_names::request_connection_blocking(api::SERVER_NAME).unwrap();
+        SERVER_CID.store(cid, Ordering::Relaxed);
+    }
+    cid
+}
+
+/// Add two numbers to gether, and return the result. Returns
+/// an Error if one is detected.
+pub fn add(arg1: i32, arg2: i32) -> Result<i32, api::Error> {
+    // Create an opcode variant. This can be a rich structure.
+    let op = api::MathOperation::Add(arg1, arg2);
+
+    // Convert the opcode into a serialized buffer. This consumes the opcode, which will
+    // exist on the heap in its own page. Furthermore, this will be in a flattened format
+    // suitable for passing around as a message.
+    let mut buf = buffer::Buffer::try_from(op).or(Err(api::Error::InternalError))?;
+
+    // Mutably lend our op to the server, specifying the `api::Opcode::Mathematics` opcode.
+    // This will return a structure that we can deserialize back into something we
+    // can verify.
+    buf.lend_mut(
+        ensure_connection(),
+        api::Opcode::Mathematics.to_u32().unwrap(),
+    )
+    .or(Err(api::Error::InternalError))?;
+
+    // Turn the result of the response into a friendly value that can be understood
+    // by the caller.
+    match buf.deserialize().unwrap() {
+        api::MathResult::Value(v) => Ok(v),
+        api::MathResult::Error(val) => Err(val),
+    }
+}
+
+/// Condensed form of the operation, that allows for reuse among other operations.
+fn do_op(op: api::MathOperation) -> Result<i32, api::Error> {
+    // Convert the opcode into a serialized buffer. This consumes the opcode, which will
+    // exist on the heap in its own page. Furthermore, this will be in a flattened format
+    // suitable for passing around as a message.
+    let mut buf = buffer::Buffer::try_from(op).or(Err(api::Error::InternalError))?;
+
+    // Lend our op to the server, specifying the `api::Opcode::Mathematics` opcode.
+    // This will return a structure that we can deserialize back into something we
+    // can verify.
+    buf.lend_mut(
+        ensure_connection(),
+        api::Opcode::Mathematics.to_u32().unwrap(),
+    )
+    .or(Err(api::Error::InternalError))?;
+    Err(api::Error::Overflow)
+}
+
+pub fn subtract(arg1: i32, arg2: i32) -> Result<i32, api::Error> {
+    do_op(api::MathOperation::Subtract(arg1, arg2))
+}
+
+pub fn multiply(arg1: i32, arg2: i32) -> Result<i32, api::Error> {
+    do_op(api::MathOperation::Multiply(arg1, arg2))
+}
+
+pub fn divide(arg1: i32, arg2: i32) -> Result<i32, api::Error> {
+    do_op(api::MathOperation::Divide(arg1, arg2))
+}
+
+pub fn log_message(prefix: &str, message: &str) {
+    let op = api::LogString {
+        prefix: xous::String::from_str(prefix),
+        message: xous::String::from_str(message),
+    };
+
+    // Convert the opcode into a serialized buffer. This consumes the opcode, which will
+    // exist on the heap in its own page. Furthermore, this will be in a flattened format
+    // suitable for passing around as a message.
+    let buf = buffer::Buffer::try_from(op).unwrap();
+
+    // Send the message to the server.
+    buf.lend(
+        ensure_connection(),
+        api::Opcode::LogString.to_u32().unwrap(),
+    )
+    .unwrap();
+}
+
+// Callback messages will be sent to this private server.
+// Ideally it would be held in a Mutex.
+static mut CALLBACK_SID: Option<xous::SID> = None;
+static mut CALLBACK_ARRAY: [Option<fn(&str, &str)>; 8] = [None; 8];
+
+// This callback server runs in its own thread.
+fn callback_server() {
+    let sid = unsafe { CALLBACK_SID }.unwrap();
+    loop {
+        let msg = xous::receive_message(sid).unwrap();
+        match FromPrimitive::from_usize(msg.body.id()) {
+            Some(api::CallbackType::LogString) => {
+                let mem = msg.body.memory_message().unwrap();
+                let buffer = unsafe { buffer::Buffer::from_memory_message(mem) };
+                let log_string = buffer.try_into::<api::LogString, _>().unwrap();
+                unsafe {
+                    for entry in CALLBACK_ARRAY.iter() {
+                        if let Some(cb) = entry {
+                            cb(log_string.message.as_str(), log_string.prefix.as_str());
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+pub fn hook_log_messages(cb: fn(&str, &str)) {
+    // Add this entry to the callback array
+    unsafe {
+        for entry in CALLBACK_ARRAY.iter_mut() {
+            if entry.is_none() {
+                *entry = Some(cb);
+                break;
+            }
+        }
+    }
+
+    // If no thread exists to handle callbacks, create one.
+    // Publish this server with the main thread so it can call us as necessary.
+    if unsafe { CALLBACK_SID }.is_none() {
+        let sid = xous::create_server().unwrap();
+        unsafe { CALLBACK_SID = Some(sid) };
+        xous::create_thread_0(callback_server).unwrap();
+        let sid = sid.to_u32();
+        xous::send_message(
+            ensure_connection(),
+            xous::Message::Scalar(xous::ScalarMessage {
+                id: api::Opcode::AddLogStringCallback.to_usize().unwrap(),
+                arg1: sid.0 as _,
+                arg2: sid.1 as _,
+                arg3: sid.2 as _,
+                arg4: sid.3 as _,
+            }),
+        )
+        .unwrap();
+    }
+}
