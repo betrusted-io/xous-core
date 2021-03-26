92656724f5dd389bedfcbc660a4669b31cc435eb
bunnie <bunnie@kosagi.com>
2021-03-26 15:53:38 +0800

    add minimal image as a build option

--- buildpush.sh
+++ buildpush.sh
@@ -12,6 +12,7 @@ LOADER_IMAGE=target/riscv32imac-unknown-none-elf/release/loader.bin
 CSR_CSV=../betrusted-soc/build/csr.csv.1
 USE_IDENTITY=0
 USE_NIGHTLY=
+IMAGE=hw-image
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -50,6 +51,10 @@ do
 	  USE_NIGHTLY=+nightly
 	  shift
 	  ;;
+      -m|--minimal)
+	  IMAGE=minimal
+	  shift
+	  ;;
       -h|--help)
 		echo "$0 provisions betrusted. --kernel-skip skips the kernel, --fpga-skip skips the FPGA. --current-csr indicates to use the CSR for the new FPGA image to do the update (normally you want to use the one corresponding to the older, currently installed version)."
 		echo "Alternatively, using --copy-to <hostname> copies the files to a remote host and skips provisioning."
@@ -73,7 +78,7 @@ DESTDIR=code/precursors
 # full rebuild, and not just an incremental rebuild. :-/
 #touch services/log-server/src/main.rs # bump the build time in the log server
 
-cargo $USE_NIGHTLY xtask hw-image ../betrusted-soc/build/software/soc.svd
+cargo $USE_NIGHTLY xtask $IMAGE ../betrusted-soc/build/software/soc.svd
 
 # only copy if changed, othrewise it seems to trigger extra build effort...
 rsync -a --no-times --checksum ../betrusted-soc/build/software/soc.svd svd2utra/examples/soc.svd
