2001f15fc361c613c52c33f3408d20337963b828
bunnie <bunnie@kosagi.com>
2021-03-26 18:15:43 +0800

    set llio default to log

--- services/llio/src/main.rs
+++ services/llio/src/main.rs
@@ -70,7 +70,7 @@ mod implementation {
         .expect("couldn't map GPIO CSR range");
         let mut gpio_csr = CSR::new(gpio_base.as_mut_ptr() as *mut u32);
         // setup the initial logging output
-        gpio_csr.wfo(utra::gpio::UARTSEL_UARTSEL, 0); // 0 = kernel, 1 = log, 2 = app_uart
+        gpio_csr.wfo(utra::gpio::UARTSEL_UARTSEL, 1); // 0 = kernel, 1 = log, 2 = app_uart
 
         gpio_base.as_mut_ptr() as *mut u32
     }
