23ce9e5bf0e02d8789d7644d364e76d81e8592de
Sean Cross <sean@xobs.io>
2021-08-16 12:19:53 +0800

    xous: arch: look for feature flag for tests
    When running tests, `processes-as-threads` gets enabled. Look for this
    feature flag to determine if we need to use that architecture.
    
    Note that this change requires `resolver v2` to be enabled.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- xous-rs/src/arch/mod.rs
+++ xous-rs/src/arch/mod.rs
@@ -1,14 +1,14 @@
 #[cfg(any(target_os = "none", target_os = "xous"))]
-pub mod native;
+pub mod riscv;
 #[cfg(any(target_os = "none", target_os = "xous"))]
-pub use native::*;
+pub use riscv::*;
 
-#[cfg(all(any(windows,unix), not(test)))]
+#[cfg(all(any(windows,unix), not(feature = "processes-as-threads")))]
 pub mod hosted;
-#[cfg(all(any(windows,unix), not(test)))]
+#[cfg(all(any(windows,unix), not(feature = "processes-as-threads")))]
 pub use hosted::*;
 
-#[cfg(all(any(windows,unix), test))]
+#[cfg(feature = "processes-as-threads")]
 pub mod test;
-#[cfg(all(any(windows,unix), test))]
+#[cfg(feature = "processes-as-threads")]
 pub use test::*;
