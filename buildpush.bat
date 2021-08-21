491e3e5429125b3e0c1cc57e07936bb6e1736f04
bunnie <bunnie@kosagi.com>
2021-08-22 02:31:52 +0800

    remove stale referecne from debugging bbram

--- buildpush.bat
+++ buildpush.bat
@@ -8,7 +8,7 @@ CertUtil -hashfile precursors/soc_csr.bin MD5
 
 CertUtil -hashfile target/riscv32imac-unknown-xous-elf/release/loader.bin MD5
 CertUtil -hashfile target/riscv32imac-unknown-xous-elf/release/xous.img MD5
-scp -i c:/users/bunnie/.ssh/id_pi target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin precursors/bbram1_soc_csr.bin precursors/bbram-test1.nky pi@10.0.245.90:code/precursors/
+scp -i c:/users/bunnie/.ssh/id_pi target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin pi@10.0.245.90:code/precursors/
 
 REM CertUtil -hashfile target/riscv32imac-unknown-none-elf/release/loader.bin MD5
 REM CertUtil -hashfile target/riscv32imac-unknown-none-elf/release/xous.img MD5
