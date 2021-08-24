69d9e38c8ba5b97045c3df268b18656a60c71ac1
bunnie <bunnie@kosagi.com>
2021-08-24 23:22:41 +0800

    add EC updates to the CI update script

--- tools/update_ci.sh
+++ tools/update_ci.sh
@@ -19,3 +19,9 @@ wget https://ci.betrusted.io/latest-ci/soc_csr.bin -O /tmp/soc_csr.bin
 ./usb_update.py -s /tmp/soc_csr.bin
 rm /tmp/soc_csr.bin
 
+echo "waiting for device to reboot"
+sleep 5
+
+wget https://ci.betrusted.io/latest-ci/bt-ec.bin -O /tmp/bt-ec.bin
+./usb_update.py -e /tmp/bt-ec.bin
+rm /tmp/bt-ec.bin
