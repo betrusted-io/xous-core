33d14b6ee37308d4973781a43e4c5c224fcb2d52
bunnie <bunnie@kosagi.com>
2021-08-24 22:59:10 +0800

    add some timeouts after each phase, for the device to reboot

--- tools/update_ci.sh
+++ tools/update_ci.sh
@@ -1,12 +1,19 @@
-#!/bin/bash set -e
+#!/bin/bash
+set -e
 
 wget https://ci.betrusted.io/latest-ci/loader.bin -O /tmp/loader.bin
 ./usb_update.py -s /tmp/loader.bin
 rm /tmp/loader.bin
 
+echo "waiting for device to reboot"
+sleep 5
+
 wget https://ci.betrusted.io/latest-ci/xous.img -O /tmp/xous.img
 ./usb_update.py -k /tmp/xous.img
 rm /tmp/xous.img
+
+echo "waiting for device to reboot"
+sleep 5
 
 wget https://ci.betrusted.io/latest-ci/soc_csr.bin -O /tmp/soc_csr.bin
 ./usb_update.py -s /tmp/soc_csr.bin
