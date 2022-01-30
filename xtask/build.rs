9573da00f412e676a9c9ea82645513ad8119d1a1
bunnie <bunnie@kosagi.com>
2022-01-31 04:12:43 +0800

    still dicking around with getting versions to appear correctly in builds

--- xtask/build.rs
+++ xtask/build.rs
@@ -2,8 +2,7 @@ use vergen::{ConstantsFlags, generate_cargo_keys};
 
 fn main() {
     // Setup the flags, toggling off the 'SEMVER_FROM_CARGO_PKG' flag
-    let mut flags = ConstantsFlags::all();
-    flags.toggle(ConstantsFlags::SEMVER_FROM_CARGO_PKG);
+    let flags = ConstantsFlags::all();
 
     // Generate the 'cargo:' key output
     generate_cargo_keys(flags).expect("Unable to generate the cargo keys!");
