2b3178be82ff413b92eacb577e83e4dab74280be
bunnie <bunnie@kosagi.com>
2022-02-28 14:07:42 +0800

    add a powershell script for bleeding edge CI
    Because i don't know how to do variables in powershell :-/

--- tools/update_ci.ps1
+++ tools/update_ci.ps1
@@ -1,6 +1,6 @@
 Write-Output "This script will update Xous and stage gateware and EC firmware for self-provisioning. No secrets will be erased."
-Write-Output "This script will use the latest release."
-$title    = 'Update to latest release'
+Write-Output "This script will use the bleeding edge CI build."
+$title    = 'Update to bleeding edge CI'
 $question = 'Are you sure you want to proceed?'
 $choices  = '&Yes', '&No'
 
@@ -11,35 +11,35 @@ if ($decision -eq 0) {
     Exit
 }
 
-Invoke-WebRequest https://ci.betrusted.io/releases/latest/loader.bin -OutFile loader.bin
+Invoke-WebRequest https://ci.betrusted.io/latest-ci/loader.bin -OutFile loader.bin
 python usb_update.py -l loader.bin
 Remove-Item loader.bin
 
 Write-Output "waiting for device to reboot"
 Start-Sleep 5
 
-Invoke-WebRequest https://ci.betrusted.io/releases/latest/xous.img -OutFile xous.img
+Invoke-WebRequest https://ci.betrusted.io/latest-ci/xous.img -OutFile xous.img
 python usb_update.py -k xous.img
 Remove-Item xous.img
 
 Write-Output "waiting for device to reboot"
 Start-Sleep 5
 
-Invoke-WebRequest https://ci.betrusted.io/releases/latest/soc_csr.bin -OutFile soc_csr.bin
+Invoke-WebRequest https://ci.betrusted.io/latest-ci/soc_csr.bin -OutFile soc_csr.bin
 python usb_update.py -s soc_csr.bin
 Remove-Item soc_csr.bin
 
 Write-Output "waiting for device to reboot"
 Start-Sleep 5
 
-Invoke-WebRequest https://ci.betrusted.io/releases/latest/ec_fw.bin -OutFile ec_fw.bin
+Invoke-WebRequest https://ci.betrusted.io/latest-ci/ec_fw.bin -OutFile ec_fw.bin
 python usb_update.py -e ec_fw.bin
 Remove-Item ec_fw.bin
 
 Write-Output "waiting for device to reboot"
 Start-Sleep 5
 
-Invoke-WebRequest https://ci.betrusted.io/releases/latest/wf200_fw.bin -OutFile wf200_fw.bin
+Invoke-WebRequest https://ci.betrusted.io/latest-ci/wf200_fw.bin -OutFile wf200_fw.bin
 python usb_update.py -w wf200_fw.bin
 Remove-Item wf200_fw.bin
 
