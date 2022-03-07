f6e9c850da10c0e1cc77aaa639bc638f0f76f131
bunnie <bunnie@kosagi.com>
2022-03-08 00:23:40 +0800

    revert inadvertent change to buildpush.ps1 by the merge

--- buildpush.ps1
+++ buildpush.ps1
@@ -3,8 +3,8 @@
 # set RUSTFLAGS=--remap-path-prefix=F:\largework\rust-win\code\xous-core\=build
 # $env:RUSTFLAGS="--remap-path-prefix=$(Get-Location)=build"
 
-# cargo xtask app-image ball repl
-cargo xtask ffi-test
+cargo xtask app-image ball repl
+# cargo xtask ffi-test
 # cargo xtask minimal precursors/soc.svd
 
 CertUtil -hashfile precursors/bbram-test1.nky MD5
