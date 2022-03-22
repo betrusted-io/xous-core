fe52756a3d565dbdc50dcfbe90e8943c213f1a33
bunnie <bunnie@kosagi.com>
2022-03-22 22:17:04 +0800

    convert the connection manager to use the new PDDB blocking call

--- services/net/src/connection_manager.rs
+++ services/net/src/connection_manager.rs
@@ -107,9 +107,7 @@ pub(crate) fn connection_manager(sid: xous::SID, activity_interval: Arc<AtomicU3
         move || {
             let tt = ticktimer_server::Ticktimer::new().unwrap();
             let pddb = pddb::Pddb::new();
-            while !pddb.is_mounted() {
-                tt.sleep_ms(1103).unwrap(); // don't pump the loop until the PDDB has been mounted, but check back regularly
-            }
+            pddb.is_mounted_blocking(None);
             loop {
                 let msg = xous::receive_message(sid).unwrap();
                 match FromPrimitive::from_usize(msg.body.id()) {
