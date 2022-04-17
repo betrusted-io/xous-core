2fbb522c06db003c9931980f2790cd5c83b5ab35
bunnie <bunnie@kosagi.com>
2022-04-17 18:50:10 +0800

    modify the web server to use mpsc, multiple workers
    This is basically the Rust example for the web server, with
    some tweaks. Add a command to make the device buzz when
    a particular URL is hit, and to wait before responding when
    another one is hit.

--- services/shellchat/src/cmds.rs
+++ services/shellchat/src/cmds.rs
@@ -47,6 +47,7 @@ pub struct CommonEnv {
     trng: Trng,
     netmgr: net::NetManager,
     xns: xous_names::XousNames,
+    boot_instant: std::time::Instant,
 }
 impl CommonEnv {
     pub fn register_handler(&mut self, verb: String::<256>) -> u32 {
@@ -163,6 +164,7 @@ impl CmdEnv {
             trng: Trng::new(&xns).unwrap(),
             xns: xous_names::XousNames::new().unwrap(),
             netmgr: net::NetManager::new(),
+            boot_instant: std::time::Instant::now(),
         };
         //let fcc = Fcc::new(&mut common);
         #[cfg(feature="benchmarks")]
