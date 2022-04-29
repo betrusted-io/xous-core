fd9710a81d9df687328e5ad2df27ec442caee4f4
Sean Cross <sean@xobs.io>
2022-04-28 22:03:53 -0400

    xous: break ProcessStartup into its own type
    This will be used when we launch a new process.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ xous-rs/src/arch/hosted/process.rs
@@ -0,0 +1,148 @@
+use super::{CHILD_PROCESS_ADDRESS};
+pub use crate::PID;
+use core::convert::{TryFrom, TryInto};
+
+/// A 16-byte random nonce that identifies this process to the kernel. This
+/// is usually provided through the environment variable `XOUS_PROCESS_KEY`.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProcessKey(pub(crate) [u8; 16]);
+impl ProcessKey {
+    pub fn new(key: [u8; 16]) -> ProcessKey {
+        ProcessKey(key)
+    }
+}
+
+/// Describes all parameters that are required to start a new process
+/// on this platform.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProcessInit {
+    pub key: ProcessKey,
+}
+
+pub struct ProcessArgs {
+    command: String,
+    name: String,
+}
+
+impl ProcessArgs {
+    pub fn new(name: &str, command: String) -> ProcessArgs {
+        ProcessArgs {
+            command,
+            name: name.to_owned(),
+        }
+    }
+}
+
+impl Into<[usize; 7]> for &ProcessInit {
+    fn into(self) -> [usize; 7] {
+        [
+            u32::from_le_bytes(self.key.0[0..4].try_into().unwrap()) as _,
+            u32::from_le_bytes(self.key.0[4..8].try_into().unwrap()) as _,
+            u32::from_le_bytes(self.key.0[8..12].try_into().unwrap()) as _,
+            u32::from_le_bytes(self.key.0[12..16].try_into().unwrap()) as _,
+            0,
+            0,
+            0,
+        ]
+    }
+}
+
+impl TryFrom<[usize; 7]> for ProcessInit {
+    type Error = crate::Error;
+    fn try_from(src: [usize; 7]) -> core::result::Result<ProcessInit, crate::Error> {
+        let mut exploded = vec![];
+        for word in src[0..4].into_iter() {
+            exploded.extend_from_slice(&(*word as u32).to_le_bytes());
+        }
+        let mut key = [0u8; 16];
+        key.copy_from_slice(&exploded);
+        Ok(ProcessInit {
+            key: ProcessKey(key),
+        })
+    }
+}
+
+/// This is returned when a process is created
+#[derive(Debug, PartialEq)]
+pub struct ProcessStartup {
+    /// The process ID of the new process
+    pid: crate::PID,
+}
+
+impl From<&[usize; 7]> for ProcessStartup {
+    fn from(src: &[usize; 7]) -> ProcessStartup {
+        ProcessStartup {
+            pid: crate::PID::new(src[0] as _).unwrap(),
+        }
+    }
+}
+
+impl From<[usize; 8]> for ProcessStartup {
+    fn from(src: [usize; 8]) -> ProcessStartup {
+        let pid = crate::PID::new(src[1] as _).unwrap();
+        ProcessStartup { pid }
+    }
+}
+
+impl Into<[usize; 7]> for &ProcessStartup {
+    fn into(self) -> [usize; 7] {
+        [self.pid.get() as _, 0, 0, 0, 0, 0, 0]
+    }
+}
+
+#[derive(Debug)]
+pub struct ProcessHandle(std::process::Child);
+
+/// If no connection exists, create a new connection to the server. This means
+/// our parent PID will be PID1. Otherwise, reuse the same connection.
+pub fn create_process_pre(_args: &ProcessArgs) -> core::result::Result<ProcessInit, crate::Error> {
+    unimplemented!()
+}
+
+/// Launch a new process with the current PID as the parent.
+pub fn create_process_post(
+    args: ProcessArgs,
+    init: ProcessInit,
+    pid: PID,
+) -> core::result::Result<ProcessHandle, crate::Error> {
+    use std::process::Command;
+    let server_env = format!("{}", CHILD_PROCESS_ADDRESS.lock().unwrap());
+    let pid_env = format!("{}", pid);
+    let process_name_env = args.name.to_string();
+    let process_key_env = hex::encode(&init.key.0);
+    let (shell, args) = if cfg!(windows) {
+        ("cmd", ["/C", &args.command])
+    } else if cfg!(unix) {
+        ("sh", ["-c", &args.command])
+    } else {
+        panic!("unrecognized platform -- don't know how to shell out");
+    };
+
+    // println!("Launching process...");
+    Command::new(shell)
+        .args(&args)
+        .env("XOUS_SERVER", server_env)
+        .env("XOUS_PID", pid_env)
+        .env("XOUS_PROCESS_NAME", process_name_env)
+        .env("XOUS_PROCESS_KEY", process_key_env)
+        .spawn()
+        .map(ProcessHandle)
+        .map_err(|_| {
+            // eprintln!("couldn't start command: {}", e);
+            crate::Error::InternalError
+        })
+}
+
+pub fn wait_process(mut joiner: ProcessHandle) -> crate::SysCallResult {
+    joiner
+        .0
+        .wait()
+        .or(Err(crate::Error::InternalError))
+        .and_then(|e| {
+            if e.success() {
+                Ok(crate::Result::Ok)
+            } else {
+                Err(crate::Error::UnknownError)
+            }
+        })
+}
---
+++ xous-rs/src/arch/riscv/process.rs
@@ -0,0 +1,129 @@
+use core::convert::TryFrom;
+
+/// ProcessArgs are the arguments that are created by the user. These
+/// will be turned into `ProcessInit` by this library prior to sending
+/// them into the kernel.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ProcessArgs {
+    name: [u8; 16],
+}
+
+/// ProcessInit describes the values that are passed to the
+/// kernel. This value will only be used internally inside
+/// `xous-rs`, as well as inside the kernel itself.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProcessInit {
+    // 0,1 -- Stack Base, Stack Size
+    stack: crate::MemoryRange,
+    // 2,3 -- Text Start, Text Size
+    text: crate::MemoryRange,
+    // 4
+    start: crate::MemoryAddress,
+    // 5
+    // 6
+}
+
+impl Into<[usize; 7]> for &ProcessInit {
+    fn into(self) -> [usize; 7] {
+        [
+            self.stack.addr.get(),
+            self.stack.size.get(),
+            self.text.addr.get(),
+            self.text.size.get(),
+            self.start.get(),
+            0,
+            0,
+        ]
+    }
+}
+
+impl TryFrom<[usize; 7]> for ProcessInit {
+    type Error = crate::Error;
+    fn try_from(src: [usize; 7]) -> Result<ProcessInit, Self::Error> {
+        Ok(ProcessInit {
+            stack: unsafe {
+                crate::MemoryRange::new(src[0], src[1]).or(Err(crate::Error::OutOfMemory))?
+            },
+            text: unsafe {
+                crate::MemoryRange::new(src[2], src[3]).or(Err(crate::Error::OutOfMemory))?
+            },
+            start: crate::MemoryAddress::new(src[4]).ok_or(crate::Error::OutOfMemory)?,
+        })
+    }
+}
+
+/// When a new process is created, this platform-specific structure is returned.
+#[derive(Debug, PartialEq)]
+pub struct ProcessStartup {
+    /// The process ID of the new process
+    pid: crate::PID,
+
+    /// A server that the parent process can connect to in order to send further
+    /// initialization messages
+    server: crate::SID,
+}
+
+impl From<&[usize; 7]> for ProcessStartup {
+    fn from(src: &[usize; 7]) -> ProcessStartup {
+        let server = crate::SID::from_array([src[1] as _, src[2] as _, src[3] as _, src[4] as _]);
+        ProcessStartup {
+            pid: crate::PID::new(src[0] as _).unwrap(),
+            server,
+        }
+    }
+}
+
+impl From<[usize; 8]> for ProcessStartup {
+    fn from(src: [usize; 8]) -> ProcessStartup {
+        let pid = crate::PID::new(src[1] as _).unwrap();
+        let server = crate::SID::from_array([src[2] as _, src[3] as _, src[4] as _, src[5] as _]);
+        ProcessStartup { pid, server }
+    }
+}
+
+impl Into<[usize; 7]> for &ProcessStartup {
+    fn into(self) -> [usize; 7] {
+        let server = self.server.to_array();
+        [
+            self.pid.get() as _,
+            server[0] as _,
+            server[1] as _,
+            server[2] as _,
+            server[3] as _,
+            0,
+            0,
+        ]
+    }
+}
+
+pub struct ProcessHandle(crate::PID);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ProcessKey([u8; 8]);
+impl ProcessKey {
+    pub fn new(key: [u8; 8]) -> ProcessKey {
+        ProcessKey(key)
+    }
+}
+
+/// Convert the `ProcessArgs` structure passed by the user into a `ProcessInit`
+/// structure suitable for consumption by the kernel.
+pub fn create_process_pre(_args: &ProcessArgs) -> core::result::Result<ProcessInit, crate::Error> {
+    todo!()
+}
+
+/// Any post-processing required to set up this process.
+pub fn create_process_post(
+    _args: ProcessArgs,
+    _init: ProcessInit,
+    pid: crate::PID,
+) -> core::result::Result<ProcessHandle, crate::Error> {
+    Ok(ProcessHandle(pid))
+}
+
+/// Wait for a process to terminate.
+pub fn wait_process(_joiner: ProcessHandle) -> crate::SysCallResult {
+    loop {
+        crate::wait_event();
+    }
+}
