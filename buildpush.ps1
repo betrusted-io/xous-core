03a191d18211489b517e13994fa297ce00b1c442
bunnie <bunnie@kosagi.com>
2022-05-17 02:27:04 +0800

    adjust IP targets on helper scripts

--- buildpush.ps1
+++ buildpush.ps1
@@ -12,7 +12,10 @@ CertUtil -hashfile precursors/soc_csr.bin MD5
 
 CertUtil -hashfile target/riscv32imac-unknown-xous-elf/release/loader.bin MD5
 CertUtil -hashfile target/riscv32imac-unknown-xous-elf/release/xous.img MD5
-scp -i c:/users/bunnie/.ssh/id_pi target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin pi@10.0.245.181:code/precursors/
+scp -i c:/users/bunnie/.ssh/id_pi target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin pi@10.0.245.135:code/precursors/
+
+# scp -i c:/users/bunnie/.ssh/id_ed25519 target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin bunnie@192.168.137.161:code/precursors/
+
 # scp -i c:/users/bunnie/.ssh/id_ed25519 target/riscv32imac-unknown-xous-elf/release/xous.img target/riscv32imac-unknown-xous-elf/release/loader.bin precursors/soc_csr.bin bunnie@10.0.245.9:/mnt/c/Users/bunnie/
 
 # CertUtil -hashfile target/riscv32imac-unknown-none-elf/release/loader.bin MD5
