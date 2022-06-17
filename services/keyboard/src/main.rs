a91292509e40aab8bc58f63734cde2682828333b
bunnie <bunnie@kosagi.com>
2022-06-18 00:15:00 +0800

    only allow the observer of the key presses to be registered once

--- services/keyboard/src/main.rs
+++ services/keyboard/src/main.rs
@@ -839,15 +839,17 @@ fn main() -> ! {
             Some(Opcode::RegisterKeyObserver) => {
                 let buffer = unsafe{Buffer::from_memory_message(msg.body.memory_message().unwrap())};
                 let kr = buffer.as_flat::<KeyboardRegistration, _>().unwrap();
-                match xns.request_connection_blocking(kr.server_name.as_str()) {
-                    Ok(cid) => {
-                        observer_conn = Some(cid);
-                        observer_op = Some(kr.listener_op_id as usize);
-                    }
-                    Err(e) => {
-                        log::error!("couldn't connect to observer: {:?}", e);
-                        observer_conn = None;
-                        observer_op = None;
+                if observer_conn.is_none() {
+                    match xns.request_connection_blocking(kr.server_name.as_str()) {
+                        Ok(cid) => {
+                            observer_conn = Some(cid);
+                            observer_op = Some(kr.listener_op_id as usize);
+                        }
+                        Err(e) => {
+                            log::error!("couldn't connect to observer: {:?}", e);
+                            observer_conn = None;
+                            observer_op = None;
+                        }
                     }
                 }
             },
