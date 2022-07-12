d3337bdaba9230bea70e59209e082c0b6066f497
bunnie <bunnie@kosagi.com>
2022-07-13 04:36:17 +0800

    make constant naming consistent (prefix indicates the address space offset)

--- xous-rs/src/definitions.rs
+++ xous-rs/src/definitions.rs
@@ -31,7 +31,7 @@ pub const EARLY_SETTINGS: u32 = 0x0097_0000;
 
 pub const KERNEL_LOC: u32 = 0x0098_0000; // kernel start
 pub const KERNEL_LEN: u32 = 0x0140_0000; // max kernel length = 0xA0_0000 * 2 => half the area for backup kernel & updates
-pub const BACKUP_OFFSET: u32 = KERNEL_LEN - 0x1000; // last page of kernel is where the backup block gets located = 0x1D7_F000
+pub const KERNEL_BACKUP_OFFSET: u32 = KERNEL_LEN - 0x1000; // last page of kernel is where the backup block gets located = 0x1D7_F000
 
 pub const EC_REGION_LOC: u32 = 0x07F8_0000; // EC update staging area. Must be aligned to a 64k-address.
 pub const EC_WF200_PKG_LOC: u32 = 0x07F8_0000;
