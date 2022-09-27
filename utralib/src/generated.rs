4f7f5855eca2d3ee76332188305475ce4e487d20
bunnie <bunnie@kosagi.com>
2022-09-27 16:32:40 +0800

    add the generated.rs top file which routes to specific configs with feature flags

---
+++ utralib/src/generated.rs
@@ -0,0 +1,18 @@
+// Feature flags are checked inside build.rs for sanity, so we
+// can use them here assuming all targets are mutually exclusive.
+
+#[cfg(feature="precursor-c809403")]
+mod precursor_c809403;
+#[cfg(feature="precursor-c809403")]
+pub use precursor_c809403::*;
+
+
+#[cfg(feature="precursor-c809403-perflib")]
+mod precursor_perf_c809403;
+#[cfg(feature="precursor-c809403-perflib")]
+pub use precursor_perf_c809403::*;
+
+// Hosted mode includes nothing, as it relies on the abstract host
+// architecture for I/O; so this file is empty when the "hosted"
+// configuration is selected and there are no corresponding "hosted"
+// mode feature flags.
