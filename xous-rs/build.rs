14b92dede415bfda48795a25ac8810d89a5a9c50
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2022-10-25 13:24:59 +0200

    Remove baremetal cfg detection in build script
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- xous-rs/build.rs
+++ xous-rs/build.rs
@@ -6,18 +6,7 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let target = env::var("TARGET").unwrap();
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let name = env::var("CARGO_PKG_NAME").unwrap();
-
-    // If we're not running on a desktop-class operating system, emit the "baremetal"
-    // config setting. This will enable software to do tasks such as
-    // managing memory.
-    if target_os == "none" {
-        println!("Target {} is bare metal", target);
-        println!("cargo:rustc-cfg=baremetal");
-    } else {
-        println!("Target {} is NOT bare metal", target);
-    }
 
     if target.starts_with("riscv") {
         fs::copy(
