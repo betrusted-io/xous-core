dfb10ed7f221ede865fe0ea2bfd79c00f82c81b3
bunnie <bunnie@kosagi.com>
2022-10-28 14:01:09 +0800

    bump crate numbers to publish lang-fr change into xous-rs

--- xous-rs/src/locale.rs
+++ xous-rs/src/locale.rs
@@ -4,9 +4,12 @@ pub const LANG: &str = "ja";
 pub const LANG: &str = "zh";
 #[cfg(feature="lang-en-tts")]
 pub const LANG: &str = "en-tts";
+#[cfg(feature="lang-fr")]
+pub const LANG: &str = "fr";
 #[cfg(not(any(
     feature="lang-ja",
     feature="lang-zh",
-    feature="lang-en-tts"
+    feature="lang-en-tts",
+    feature="lang-fr"
 )))]
 pub const LANG: &str = "en";
