55dca017de71f4d76dc092a4fbde35da70dc6e75
Tom Marble <tmarble@info9.net>
2023-01-13 16:52:28 -0600

    WIP: Fix mtxcli excessive memory usage #287
    Adds migrations framework
    - Migration for v0.9.11-0120: update the filter
    
    Filter
    - narrow filter to only the fields and events for the current room
    
    Introduces "read only" variables (e.g. __version).
    
    Signed-off-by: Tom Marble <tmarble@info9.net>

---
+++ apps/mtxcli/src/cmds/migrations/v0_9_11_0120.rs
@@ -0,0 +1,26 @@
+use std::io::Error;
+
+use crate::cmds::{CommonEnv,FILTER_KEY};
+use crate::cmds::migrations::MigrationApi;
+use crate::migration_api;
+
+#[derive(Debug)]
+pub struct V0_9_11_0120 {
+}
+impl V0_9_11_0120 {
+    pub fn new() -> Self {
+        V0_9_11_0120 {
+        }
+    }
+}
+
+impl<'a> MigrationApi<'a> for V0_9_11_0120 {
+    migration_api!("v0.9.11-0120");
+
+    fn process(&self, common: &mut CommonEnv) -> Result<bool, Error> {
+        log::info!("Running migration for: {}", self.version());
+        // we need to provoke the creation of a new/updated filter
+        common.unset(FILTER_KEY)?;
+        Ok(true)
+    }
+}
