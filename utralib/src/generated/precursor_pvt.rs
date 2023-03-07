b36290fd79f7eed37b70f2130c6f49f6b63efe9c
bunnie <bunnie@kosagi.com>
2023-03-08 04:02:36 +0800

    update the precursor_pvt.rs file with the autogen version to avoid errors on publish

--- utralib/src/generated/precursor_pvt.rs
+++ utralib/src/generated/precursor_pvt.rs
@@ -320,13 +320,6 @@ pub const HW_WFI_BASE :   usize = 0xf0025000;
 pub mod utra {
 
     pub mod reboot {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum RebootOffset {
-            SocReset = 0,
-            Addr = 4,
-            CpuReset = 8,
-        }
         pub const REBOOT_NUMREGS: usize = 3;
 
         pub const SOC_RESET: crate::Register = crate::Register::new(0, 0xff);
@@ -342,16 +335,6 @@ pub mod utra {
     }
 
     pub mod timer0 {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum Timer0Offset {
-            Load = 0,
-            Reload = 4,
-            En = 8,
-            EvStatus = 12,
-            EvPending = 16,
-            EvEnable = 20,
-        }
         pub const TIMER0_NUMREGS: usize = 6;
 
         pub const LOAD: crate::Register = crate::Register::new(0, 0xffffffff);
@@ -377,20 +360,6 @@ pub mod utra {
     }
 
     pub mod gpio {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum GpioOffset {
-            Output = 0,
-            Input = 4,
-            Drive = 8,
-            Intena = 12,
-            Intpol = 16,
-            Uartsel = 20,
-            Debug = 24,
-            EvStatus = 28,
-            EvPending = 32,
-            EvEnable = 36,
-        }
         pub const GPIO_NUMREGS: usize = 10;
 
         pub const OUTPUT: crate::Register = crate::Register::new(0, 0xff);
@@ -450,16 +419,6 @@ pub mod utra {
     }
 
     pub mod uart {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum UartOffset {
-            Rxtx = 0,
-            Txfull = 4,
-            Rxempty = 8,
-            EvStatus = 12,
-            EvPending = 16,
-            EvEnable = 20,
-        }
         pub const UART_NUMREGS: usize = 6;
 
         pub const RXTX: crate::Register = crate::Register::new(0, 0xff);
@@ -488,16 +447,6 @@ pub mod utra {
     }
 
     pub mod console {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum ConsoleOffset {
-            Rxtx = 0,
-            Txfull = 4,
-            Rxempty = 8,
-            EvStatus = 12,
-            EvPending = 16,
-            EvEnable = 20,
-        }
         pub const CONSOLE_NUMREGS: usize = 6;
 
         pub const RXTX: crate::Register = crate::Register::new(0, 0xff);
@@ -526,22 +475,6 @@ pub mod utra {
     }
 
     pub mod info {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum InfoOffset {
-            DnaId1 = 0,
-            DnaId0 = 4,
-            GitMajor = 8,
-            GitMinor = 12,
-            GitRevision = 16,
-            GitGitrev = 20,
-            GitGitextra = 24,
-            GitDirty = 28,
-            PlatformPlatform1 = 32,
-            PlatformPlatform0 = 36,
-            PlatformTarget1 = 40,
-            PlatformTarget0 = 44,
-        }
         pub const INFO_NUMREGS: usize = 12;
 
         pub const DNA_ID1: crate::Register = crate::Register::new(0, 0xffffffff);
@@ -584,18 +517,6 @@ pub mod utra {
     }
 
     pub mod memlcd {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum MemlcdOffset {
-            Command = 0,
-            Busy = 4,
-            Prescaler = 8,
-            EvStatus = 12,
-            EvPending = 16,
-            EvEnable = 20,
-            Devboot = 24,
-            Devstatus = 28,
-        }
         pub const MEMLCD_NUMREGS: usize = 8;
 
         pub const COMMAND: crate::Register = crate::Register::new(0, 0x3);
@@ -627,17 +548,6 @@ pub mod utra {
     }
 
     pub mod com {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum ComOffset {
-            Tx = 0,
-            Rx = 4,
-            Control = 8,
-            Status = 12,
-            EvStatus = 16,
-            EvPending = 20,
-            EvEnable = 24,
-        }
         pub const COM_NUMREGS: usize = 7;
 
         pub const TX: crate::Register = crate::Register::new(0, 0xffff);
@@ -671,20 +581,6 @@ pub mod utra {
     }
 
     pub mod i2c {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum I2COffset {
-            Prescale = 0,
-            Control = 4,
-            Txr = 8,
-            Rxr = 12,
-            Command = 16,
-            Status = 20,
-            CoreReset = 24,
-            EvStatus = 28,
-            EvPending = 32,
-            EvEnable = 36,
-        }
         pub const I2C_NUMREGS: usize = 10;
 
         pub const PRESCALE: crate::Register = crate::Register::new(0, 0xffff);
@@ -738,13 +634,6 @@ pub mod utra {
     }
 
     pub mod btevents {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum BteventsOffset {
-            EvStatus = 0,
-            EvPending = 4,
-            EvEnable = 8,
-        }
         pub const BTEVENTS_NUMREGS: usize = 3;
 
         pub const EV_STATUS: crate::Register = crate::Register::new(0, 0x3);
@@ -764,18 +653,6 @@ pub mod utra {
     }
 
     pub mod ticktimer {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum TicktimerOffset {
-            Control = 0,
-            Time1 = 4,
-            Time0 = 8,
-            MsleepTarget1 = 12,
-            MsleepTarget0 = 16,
-            EvStatus = 20,
-            EvPending = 24,
-            EvEnable = 28,
-        }
         pub const TICKTIMER_NUMREGS: usize = 8;
 
         pub const CONTROL: crate::Register = crate::Register::new(0, 0x1);
@@ -807,23 +684,6 @@ pub mod utra {
     }
 
     pub mod susres {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum SusresOffset {
-            Control = 0,
-            ResumeTime1 = 4,
-            ResumeTime0 = 8,
-            Time1 = 12,
-            Time0 = 16,
-            Status = 20,
-            State = 24,
-            Powerdown = 28,
-            Wfi = 32,
-            Interrupt = 36,
-            EvStatus = 40,
-            EvPending = 44,
-            EvEnable = 48,
-        }
         pub const SUSRES_NUMREGS: usize = 13;
 
         pub const CONTROL: crate::Register = crate::Register::new(0, 0x3);
@@ -872,19 +732,6 @@ pub mod utra {
     }
 
     pub mod power {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum PowerOffset {
-            Power = 0,
-            ClkStatus = 4,
-            WakeupSource = 8,
-            ActivityRate = 12,
-            SamplingPeriod = 16,
-            Vibe = 20,
-            EvStatus = 24,
-            EvPending = 28,
-            EvEnable = 32,
-        }
         pub const POWER_NUMREGS: usize = 9;
 
         pub const POWER: crate::Register = crate::Register::new(0, 0xfff);
@@ -943,14 +790,6 @@ pub mod utra {
     }
 
     pub mod spinor_soft_int {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum SpinorSoftIntOffset {
-            EvStatus = 0,
-            EvPending = 4,
-            EvEnable = 8,
-            Softint = 12,
-        }
         pub const SPINOR_SOFT_INT_NUMREGS: usize = 4;
 
         pub const EV_STATUS: crate::Register = crate::Register::new(0, 0x1);
@@ -970,23 +809,6 @@ pub mod utra {
     }
 
     pub mod spinor {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum SpinorOffset {
-            Config = 0,
-            DelayConfig = 4,
-            DelayStatus = 8,
-            Command = 12,
-            CmdArg = 16,
-            CmdRbkData = 20,
-            Status = 24,
-            Wdata = 28,
-            EvStatus = 32,
-            EvPending = 36,
-            EvEnable = 40,
-            EccAddress = 44,
-            EccStatus = 48,
-        }
         pub const SPINOR_NUMREGS: usize = 13;
 
         pub const CONFIG: crate::Register = crate::Register::new(0, 0x1f);
@@ -1041,23 +863,6 @@ pub mod utra {
     }
 
     pub mod keyboard {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum KeyboardOffset {
-            UartChar = 0,
-            Row0Dat = 4,
-            Row1Dat = 8,
-            Row2Dat = 12,
-            Row3Dat = 16,
-            Row4Dat = 20,
-            Row5Dat = 24,
-            Row6Dat = 28,
-            Row7Dat = 32,
-            Row8Dat = 36,
-            EvStatus = 40,
-            EvPending = 44,
-            EvEnable = 48,
-        }
         pub const KEYBOARD_NUMREGS: usize = 13;
 
         pub const UART_CHAR: crate::Register = crate::Register::new(0, 0x1ff);
@@ -1108,12 +913,6 @@ pub mod utra {
     }
 
     pub mod keyinject {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum KeyinjectOffset {
-            UartChar = 0,
-            Disable = 4,
-        }
         pub const KEYINJECT_NUMREGS: usize = 2;
 
         pub const UART_CHAR: crate::Register = crate::Register::new(0, 0xff);
@@ -1126,12 +925,6 @@ pub mod utra {
     }
 
     pub mod seed {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum SeedOffset {
-            Seed1 = 0,
-            Seed0 = 4,
-        }
         pub const SEED_NUMREGS: usize = 2;
 
         pub const SEED1: crate::Register = crate::Register::new(0, 0xffffffff);
@@ -1144,14 +937,6 @@ pub mod utra {
     }
 
     pub mod keyrom {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum KeyromOffset {
-            Address = 0,
-            Data = 4,
-            Lockaddr = 8,
-            Lockstat = 12,
-        }
         pub const KEYROM_NUMREGS: usize = 4;
 
         pub const ADDRESS: crate::Register = crate::Register::new(0, 0xff);
@@ -1170,19 +955,6 @@ pub mod utra {
     }
 
     pub mod audio {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum AudioOffset {
-            EvStatus = 0,
-            EvPending = 4,
-            EvEnable = 8,
-            RxCtl = 12,
-            RxStat = 16,
-            RxConf = 20,
-            TxCtl = 24,
-            TxStat = 28,
-            TxConf = 32,
-        }
         pub const AUDIO_NUMREGS: usize = 9;
 
         pub const EV_STATUS: crate::Register = crate::Register::new(0, 0xf);
@@ -1247,17 +1019,6 @@ pub mod utra {
     }
 
     pub mod trng_kernel {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum TrngKernelOffset {
-            Status = 0,
-            Data = 4,
-            Urandom = 8,
-            UrandomValid = 12,
-            EvStatus = 16,
-            EvPending = 20,
-            EvEnable = 24,
-        }
         pub const TRNG_KERNEL_NUMREGS: usize = 7;
 
         pub const STATUS: crate::Register = crate::Register::new(0, 0x3fffff);
@@ -1292,68 +1053,6 @@ pub mod utra {
     }
 
     pub mod trng_server {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum TrngServerOffset {
-            Control = 0,
-            Data = 4,
-            Status = 8,
-            AvConfig = 12,
-            RoConfig = 16,
-            AvNist = 20,
-            RoNist = 24,
-            Underruns = 28,
-            NistErrors = 32,
-            NistRoStat0 = 36,
-            NistRoStat1 = 40,
-            NistRoStat2 = 44,
-            NistRoStat3 = 48,
-            NistAvStat0 = 52,
-            NistAvStat1 = 56,
-            RoRunslimit1 = 60,
-            RoRunslimit2 = 64,
-            RoRunslimit3 = 68,
-            RoRunslimit4 = 72,
-            RoRun0Ctrl = 76,
-            RoRun0Fresh = 80,
-            RoRun0Count1 = 84,
-            RoRun0Count2 = 88,
-            RoRun0Count3 = 92,
-            RoRun0Count4 = 96,
-            RoRun1Ctrl = 100,
-            RoRun1Fresh = 104,
-            RoRun1Count1 = 108,
-            RoRun1Count2 = 112,
-            RoRun1Count3 = 116,
-            RoRun1Count4 = 120,
-            RoRun2Ctrl = 124,
-            RoRun2Fresh = 128,
-            RoRun2Count1 = 132,
-            RoRun2Count2 = 136,
-            RoRun2Count3 = 140,
-            RoRun2Count4 = 144,
-            RoRun3Ctrl = 148,
-            RoRun3Fresh = 152,
-            RoRun3Count1 = 156,
-            RoRun3Count2 = 160,
-            RoRun3Count3 = 164,
-            RoRun3Count4 = 168,
-            AvExcursion0Ctrl = 172,
-            AvExcursion0Stat = 176,
-            AvExcursion0LastErr = 180,
-            AvExcursion1Ctrl = 184,
-            AvExcursion1Stat = 188,
-            AvExcursion1LastErr = 192,
-            Ready = 196,
-            EvStatus = 200,
-            EvPending = 204,
-            EvEnable = 208,
-            Chacha = 212,
-            Seed = 216,
-            Urandom = 220,
-            UrandomValid = 224,
-            Test = 228,
-        }
         pub const TRNG_SERVER_NUMREGS: usize = 58;
 
         pub const CONTROL: crate::Register = crate::Register::new(0, 0x1f);
@@ -1597,30 +1296,6 @@ pub mod utra {
     }
 
     pub mod trng {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum TrngOffset {
-            XadcTemperature = 0,
-            XadcVccint = 4,
-            XadcVccaux = 8,
-            XadcVccbram = 12,
-            XadcVbus = 16,
-            XadcUsbP = 20,
-            XadcUsbN = 24,
-            XadcNoise0 = 28,
-            XadcNoise1 = 32,
-            XadcEoc = 36,
-            XadcEos = 40,
-            XadcGpio5 = 44,
-            XadcGpio2 = 48,
-            XadcDrpEnable = 52,
-            XadcDrpRead = 56,
-            XadcDrpWrite = 60,
-            XadcDrpDrdy = 64,
-            XadcDrpAdr = 68,
-            XadcDrpDatW = 72,
-            XadcDrpDatR = 76,
-        }
         pub const TRNG_NUMREGS: usize = 20;
 
         pub const XADC_TEMPERATURE: crate::Register = crate::Register::new(0, 0xfff);
@@ -1687,35 +1362,6 @@ pub mod utra {
     }
 
     pub mod sha512 {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum Sha512Offset {
-            Power = 0,
-            Config = 4,
-            Command = 8,
-            Digest01 = 12,
-            Digest00 = 16,
-            Digest11 = 20,
-            Digest10 = 24,
-            Digest21 = 28,
-            Digest20 = 32,
-            Digest31 = 36,
-            Digest30 = 40,
-            Digest41 = 44,
-            Digest40 = 48,
-            Digest51 = 52,
-            Digest50 = 56,
-            Digest61 = 60,
-            Digest60 = 64,
-            Digest71 = 68,
-            Digest70 = 72,
-            MsgLength1 = 76,
-            MsgLength0 = 80,
-            EvStatus = 84,
-            EvPending = 88,
-            EvEnable = 92,
-            Fifo = 96,
-        }
         pub const SHA512_NUMREGS: usize = 25;
 
         pub const POWER: crate::Register = crate::Register::new(0, 0x1);
@@ -1816,21 +1462,6 @@ pub mod utra {
     }
 
     pub mod engine {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum EngineOffset {
-            Window = 0,
-            Mpstart = 4,
-            Mplen = 8,
-            Control = 12,
-            Mpresume = 16,
-            Power = 20,
-            Status = 24,
-            EvStatus = 28,
-            EvPending = 32,
-            EvEnable = 36,
-            Instruction = 40,
-        }
         pub const ENGINE_NUMREGS: usize = 11;
 
         pub const WINDOW: crate::Register = crate::Register::new(0, 0xf);
@@ -1883,12 +1514,6 @@ pub mod utra {
     }
 
     pub mod jtag {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum JtagOffset {
-            Next = 0,
-            Tdo = 4,
-        }
         pub const JTAG_NUMREGS: usize = 2;
 
         pub const NEXT: crate::Register = crate::Register::new(0, 0x3);
@@ -1903,13 +1528,6 @@ pub mod utra {
     }
 
     pub mod wdt {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum WdtOffset {
-            Watchdog = 0,
-            Period = 4,
-            State = 8,
-        }
         pub const WDT_NUMREGS: usize = 3;
 
         pub const WATCHDOG: crate::Register = crate::Register::new(0, 0x3);
@@ -1929,15 +1547,6 @@ pub mod utra {
     }
 
     pub mod usbdev {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum UsbdevOffset {
-            Usbdisable = 0,
-            Usbselect = 4,
-            EvStatus = 8,
-            EvPending = 12,
-            EvEnable = 16,
-        }
         pub const USBDEV_NUMREGS: usize = 5;
 
         pub const USBDISABLE: crate::Register = crate::Register::new(0, 0x1);
@@ -1961,12 +1570,6 @@ pub mod utra {
     }
 
     pub mod d11ctime {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum D11CtimeOffset {
-            Control = 0,
-            Heartbeat = 4,
-        }
         pub const D11CTIME_NUMREGS: usize = 2;
 
         pub const CONTROL: crate::Register = crate::Register::new(0, 0x7fff);
@@ -1979,12 +1582,6 @@ pub mod utra {
     }
 
     pub mod wfi {
-        // this enum is vestigal, and currently not used by anything
-        #[derive(Debug, Copy, Clone)]
-        pub enum WfiOffset {
-            Wfi = 0,
-            IgnoreLocked = 4,
-        }
         pub const WFI_NUMREGS: usize = 2;
 
         pub const WFI: crate::Register = crate::Register::new(0, 0x1);
