344dd5094b64286294f370f6c305273725a7c0c5
bunnie <bunnie@kosagi.com>
2023-04-10 15:41:59 +0800

    make the LCD panic handler Precursor-specific

--- kernel/src/arch/riscv/panic.rs
+++ kernel/src/arch/riscv/panic.rs
@@ -5,24 +5,27 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn handle_panic(_arg: &PanicInfo) -> ! {
     println!("PANIC in PID {}: {}", crate::arch::current_pid(), _arg);
-    use crate::platform::precursor::lcdpanic::ErrorWriter;
-    use core::fmt::Write;
-    if let Ok(mut writer) = ErrorWriter::new() {
-        writeln!(
-            writer,
-            "PANIC in PID {}: {}",
-            crate::arch::current_pid(),
-            _arg
-        )
-        .ok();
-        let process = crate::arch::process::Process::current();
-        writeln!(writer, "Current thread: {}", process.current_tid()).ok();
-        writeln!(
-            writer,
-            "{}",
-            crate::arch::process::Process::current().current_thread()
-        )
-        .ok();
+    #[cfg(any(feature = "precursor", feature = "renode"))]
+    {
+        use crate::platform::precursor::lcdpanic::ErrorWriter;
+        use core::fmt::Write;
+        if let Ok(mut writer) = ErrorWriter::new() {
+            writeln!(
+                writer,
+                "PANIC in PID {}: {}",
+                crate::arch::current_pid(),
+                _arg
+            )
+            .ok();
+            let process = crate::arch::process::Process::current();
+            writeln!(writer, "Current thread: {}", process.current_tid()).ok();
+            writeln!(
+                writer,
+                "{}",
+                crate::arch::process::Process::current().current_thread()
+            )
+            .ok();
+        }
     }
     loop {
         arch::idle();
