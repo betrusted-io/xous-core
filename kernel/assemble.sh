fb710592b016aa1e7b8fbce439f9f27c49564eb5
bunnie <bunnie@kosagi.com>
2023-04-27 22:09:08 +0800

    harmonize scripts around xpack distro (goodbye sifive)

--- kernel/assemble.sh
+++ kernel/assemble.sh
@@ -34,15 +34,15 @@ case $arch in
         # files to the old blobs
         rm -f bin/riscv*.a
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac src/arch/riscv/asm.S -o bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac src/arch/riscv/asm.S -o bin/$crate.o
         ar crs bin/riscv32imac-unknown-none-elf.a bin/$crate.o
         ar crs bin/riscv32imac-unknown-xous-elf.a bin/$crate.o
         ar crs bin/riscv32imc-unknown-none-elf.a bin/$crate.o
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i src/arch/riscv/asm.S -DSKIP_MULTICORE -o bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i src/arch/riscv/asm.S -DSKIP_MULTICORE -o bin/$crate.o
         ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac src/arch/riscv/asm.S -o bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac src/arch/riscv/asm.S -o bin/$crate.o
         ar crs bin/riscv64imac-unknown-none-elf.a bin/$crate.o
         ar crs bin/riscv64gc-unknown-none-elf.a bin/$crate.o
         ;;
