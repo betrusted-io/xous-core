18f2017bf90d5bf4e328113eb5a993af9c84ad16
bunnie <bunnie@kosagi.com>
2023-05-22 02:31:28 +0800

    add autoumount, autosleep options to userprefs

--- libs/userprefs/src/lib.rs
+++ libs/userprefs/src/lib.rs
@@ -48,6 +48,8 @@ pub struct UserPrefs {
     pub connect_known_networks_on_boot: bool,
     pub autobacklight_on_boot: bool,
     pub autobacklight_timeout: u64,
+    pub autosleep_timeout: u64,
+    pub autounmount_timeout: u64,
     pub audio_enabled: bool,
     pub earpiece_volume: u32,
     pub headset_volume: u32,
