23a9b0cc342a5c1492a5cfe177172025e2976cd6
bunnie <bunnie@kosagi.com>
2023-12-24 22:14:39 +0800

    add more helpful debug output for token claim failure

--- services/gam/src/tokens.rs
+++ services/gam/src/tokens.rs
@@ -42,9 +42,20 @@ impl<'a> TokenManager {
         } else {
             // throw a bone to the dev who has to debug this error. This typically only triggers after a major
             // refactor and some UX element was removed and we forgot to update it in this table here.
-            log::info!("Occupied token slots:");
+            log::info!("Occupied token slots: ***");
             for t in self.tokens.iter() {
-                log::info!("{}", t.name);
+                log::info!("  {}", t.name);
+            }
+            log::info!("Expected token slots:");
+            for t in EXPECTED_BOOT_CONTEXTS.iter() {
+                log::info!("  {}", t);
+            }
+            for t in EXPECTED_APP_CONTEXTS.iter() {
+                log::info!("  {}", t);
+            }
+            #[cfg(feature="unsafe-app-loading")]
+            for t in self.extra_names.iter() {
+                log::info!("{}", t);
             }
             false
         }
