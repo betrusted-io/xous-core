124040bbb3775e5faf5fce6e867f971d9b7447ea
Sean Cross <sean@xobs.io>
2023-12-29 18:15:48 +0800

    kernel: asm: align kernel stack to 16 bytes
    The kernel stack offset should be aligned to 16 bytes. Previously it was
    aligned to 8 bytes. It's unclear if this was the cause of any issues,
    but it is best practice to keep things aligned to 16 bytes.
    
    As part of this, key the atomic instructions off of the CPP macro
    presence of atomic instructions.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- kernel/assemble.ps1
+++ kernel/assemble.ps1
@@ -13,17 +13,17 @@ Switch ($Arch) {
         # remove existing blobs because otherwise this will append object files to the old blobs
         Remove-Item -Force bin/riscv*.a
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac src/arch/riscv/asm.S -o bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv32imac-unknown-none-elf.a bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv32imac-unknown-xous-elf.a bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv32imc-unknown-none-elf.a bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac_zicsr_zifencei src/arch/riscv/asm.S -o bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv32imac-unknown-none-elf.a bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv32imac-unknown-xous-elf.a bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv32imc-unknown-none-elf.a bin/$crate.o
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i src/arch/riscv/asm.S -DSKIP_MULTICORE -o bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i_zicsr_zifencei src/arch/riscv/asm.S -o bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
 
-        riscv64-unknown-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac src/arch/riscv/asm.S -o bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv64imac-unknown-none-elf.a bin/$crate.o
-        riscv64-unknown-elf-ar crs bin/riscv64gc-unknown-none-elf.a bin/$crate.o
+        riscv-none-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac_zicsr_zifencei src/arch/riscv/asm.S -o bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv64imac-unknown-none-elf.a bin/$crate.o
+        riscv-none-elf-ar crs bin/riscv64gc-unknown-none-elf.a bin/$crate.o
     }
     "arm" {
         Remove-Item -Force bin/arm*.a
