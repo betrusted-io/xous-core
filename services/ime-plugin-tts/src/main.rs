411e0ca9d8c95934c8b10a5f4349d7ff119c8e44
bunnie <bunnie@kosagi.com>
2024-01-14 22:15:37 +0800

    format services/ime-plugin-tts

--- services/ime-plugin-tts/src/main.rs
+++ services/ime-plugin-tts/src/main.rs
@@ -2,11 +2,10 @@
 #![cfg_attr(target_os = "none", no_main)]
 
 use ime_plugin_api::*;
-
-use xous_ipc::{String, Buffer};
 use num_traits::FromPrimitive;
 use tts_frontend::*;
 use xous::msg_scalar_unpack;
+use xous_ipc::{Buffer, String};
 
 fn main() -> ! {
     log_server::init_wait().unwrap();
@@ -15,15 +14,12 @@ fn main() -> ! {
 
     let xns = xous_names::XousNames::new().unwrap();
     // one connection only, should be the GAM
-    let ime_sh_sid = xns.register_name(ime_plugin_tts::SERVER_NAME_IME_PLUGIN_TTS, None).expect("can't register server");
+    let ime_sh_sid =
+        xns.register_name(ime_plugin_tts::SERVER_NAME_IME_PLUGIN_TTS, None).expect("can't register server");
     log::trace!("registered with NS -- {:?}", ime_sh_sid);
     let tts = TtsFrontend::new(&xns).unwrap();
 
-    let mytriggers = PredictionTriggers {
-        newline: true,
-        punctuation: true,
-        whitespace: true,
-    };
+    let mytriggers = PredictionTriggers { newline: true, punctuation: true, whitespace: true };
 
     log::trace!("ready to accept requests");
     let mut api_token: Option<[u32; 4]> = None;
@@ -31,9 +27,8 @@ fn main() -> ! {
         let mut msg = xous::receive_message(ime_sh_sid).unwrap();
         match FromPrimitive::from_usize(msg.body.id()) {
             Some(Opcode::Acquire) => {
-                let mut buffer = unsafe {
-                    Buffer::from_memory_message_mut(msg.body.memory_message_mut().unwrap())
-                };
+                let mut buffer =
+                    unsafe { Buffer::from_memory_message_mut(msg.body.memory_message_mut().unwrap()) };
                 let mut ret = buffer.to_original::<AcquirePredictor, _>().unwrap();
                 if api_token.is_none() {
                     if let Some(token) = ret.token {
@@ -61,31 +56,34 @@ fn main() -> ! {
                     log::warn!("Release called on a predictor that was in a released state");
                 }
             }),
-            Some(Opcode::Input) => {
-            }
+            Some(Opcode::Input) => {}
             Some(Opcode::Picked) => {
                 let buffer = unsafe { Buffer::from_memory_message(msg.body.memory_message().unwrap()) };
-                let s = buffer.as_flat::<String::<4000>, _>().unwrap();
+                let s = buffer.as_flat::<String<4000>, _>().unwrap();
                 tts.tts_simple(s.as_str()).unwrap();
             }
             Some(Opcode::Prediction) => {
                 // we don't check the API token, because we always return `false`
-                let mut buffer = unsafe { Buffer::from_memory_message_mut(msg.body.memory_message_mut().unwrap()) };
+                let mut buffer =
+                    unsafe { Buffer::from_memory_message_mut(msg.body.memory_message_mut().unwrap()) };
                 let mut prediction: Prediction = buffer.to_original::<Prediction, _>().unwrap();
                 prediction.valid = false;
                 buffer.replace(Return::Prediction(prediction)).expect("couldn't return Prediction");
             }
-            Some(Opcode::Unpick) => {
-            }
+            Some(Opcode::Unpick) => {}
             Some(Opcode::GetPredictionTriggers) => {
-                xous::return_scalar(msg.sender, mytriggers.into()).expect("couldn't return GetPredictionTriggers");
+                xous::return_scalar(msg.sender, mytriggers.into())
+                    .expect("couldn't return GetPredictionTriggers");
             }
             Some(Opcode::Quit) => {
                 if api_token.is_some() {
-                    log::error!("received quit, goodbye!"); break;
+                    log::error!("received quit, goodbye!");
+                    break;
                 }
             }
-            None => {log::error!("unknown Opcode");}
+            None => {
+                log::error!("unknown Opcode");
+            }
         }
     }
     log::trace!("main loop exit, destroying servers");
