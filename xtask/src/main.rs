5d0180cd35d8eea97244d2fdb8d178740146d9cb
Sean Cross <sean@xobs.io>
2024-02-06 10:34:16 +0800

    xtask: build kernel and loader with different target
    The kernel and loader may need to use a different target, particularly
    in the case of Precursor where the kernel and loader need an OS of
    `none`.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -42,8 +42,10 @@ const MIN_XOUS_VERSION: &str = "v0.9.8-791";
 
 /// target triple for precursor builds
 pub(crate) const TARGET_TRIPLE_RISCV32: &str = "riscv32imac-unknown-xous-elf";
+pub(crate) const TARGET_TRIPLE_RISCV32_KERNEL: &str = "riscv32imac-unknown-none-elf";
 /// target triple for ARM builds
 pub(crate) const TARGET_TRIPLE_ARM: &str = "armv7a-unknown-xous-elf";
+pub(crate) const TARGET_TRIPLE_ARM_KERNEL: &str = "armv7a-unknown-none-elf";
 
 // because I have nowhere else to note this. The commit that contains the rkyv-enum derive
 // refactor to work around warnings thrown by Rust 1.64.0 is: f815ed85b58b671178fbf53b4cea34186fc406eb
