bb2b7f05e7c4133c8bc6906c207983aa70833b91
nworbnhoj <john@nhoj.info>
2024-02-27 07:22:11 +1000

    fix tls::ota.pddb_key()

--- libs/tls/src/ota.rs
+++ libs/tls/src/ota.rs
@@ -28,13 +28,7 @@ impl OwnedTrustAnchor {
     }
 
     pub fn pddb_key(&self) -> String {
-        let subject = match std::str::from_utf8(&self.subject) {
-            Ok(subject) => subject,
-            Err(e) => {
-                log::warn!("failed to extract ota.subject: {e}");
-                ""
-            }
-        };
+        let subject = self.subject();
         let begin = match subject.find("CN=") {
             Some(begin) => Some(begin),
             None => subject.find("OU="),
