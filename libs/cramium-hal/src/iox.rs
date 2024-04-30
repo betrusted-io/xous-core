436be7ae9c9122d5f277d755bf7c29e408082dfc
bunnie <bunnie@kosagi.com>
2024-04-30 17:10:28 +0800

    fix masking bug in iox implementation

--- libs/cramium-hal/src/iox.rs
+++ libs/cramium-hal/src/iox.rs
@@ -126,7 +126,7 @@ impl Iox {
             let oe_ptr = self.csr.base();
             IoxValue::from(
                 oe_ptr.add(iox::SFR_GPIOIN_SRGI0.offset() + port as usize).read_volatile()
-                    & !(1u32 << (pin as u32)),
+                    & (1u32 << (pin as u32)),
             )
         }
     }
