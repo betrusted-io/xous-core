0b6397961450640ef957e9ab986cdf2de9b45c57
bunnie <bunnie@kosagi.com>
2024-05-04 01:45:10 +0800

    fix visibility of app uart for cramium config

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -2,3 +2,11 @@
 
 #[cfg(feature = "swap")]
 pub mod swap;
+
+// Locate the hard-wired IFRAM allocations for UDMA
+#[allow(dead_code)]
+#[cfg(feature = "cramium-soc")]
+pub const UART_IFRAM_ADDR: usize = utralib::HW_IFRAM0_MEM + utralib::HW_IFRAM0_MEM_LEN - 4096;
+#[allow(dead_code)]
+#[cfg(feature = "cramium-soc")]
+pub const APP_UART_IFRAM_ADDR: usize = utralib::HW_IFRAM0_MEM + utralib::HW_IFRAM0_MEM_LEN - 3 * 4096;
