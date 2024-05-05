fe0db3e29a44189e52a9f6b60d946deb9b06500f
bunnie <bunnie@kosagi.com>
2024-05-05 21:50:09 +0800

    more stack space is needed due to the increased size of the RPT

--- loader/src/consts.rs
+++ loader/src/consts.rs
@@ -17,7 +17,7 @@ pub const KERNEL_ARGUMENT_OFFSET: usize = 0xffc0_0000;
 #[cfg(not(feature = "swap"))]
 pub const GUARD_MEMORY_BYTES: usize = 3 * crate::PAGE_SIZE;
 #[cfg(feature = "swap")]
-pub const GUARD_MEMORY_BYTES: usize = 4 * crate::PAGE_SIZE; // this is bare minimum. Increase more if we want to do resume!
+pub const GUARD_MEMORY_BYTES: usize = 7 * crate::PAGE_SIZE; // this is bare minimum. Increase more if we want to do resume!
 
 #[cfg(feature = "swap")]
 pub const SWAPPER_PID: u8 = 2;
