a081d6cd5d28cc58abb7fa036adfccee09766bc8
bunnie <bunnie@kosagi.com>
2024-06-19 22:04:30 +0800

    note benchmark results

--- services/cram-console/src/cmds/mbox.rs
+++ services/cram-console/src/cmds/mbox.rs
@@ -259,6 +259,10 @@ impl<'a> ShellCmdApi<'a> for Mbox {
                         }
                     };
                 }
+                // Time trial results
+                //   - on Precursor (100MHz): 27.60 seconds
+                //   - RV32 Daric (400MHz): 10.11 seconds (2.72x over Precursor)
+                //   - CM7 Daric (400MHz): 3.66 seconds (2.76x over RV32; 7.54x over Precursor)
                 "clifford" => {
                     log::info!("prefill");
                     let prefill = unsafe { self.phys_mem.as_slice_mut() };
