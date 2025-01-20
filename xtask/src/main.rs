b23909cb2ad6c4a2ea613294a8147bae2223d23e
bunnie <bunnie@kosagi.com>
2025-01-20 15:27:47 +0800

    reconfigure xtask for better demo presentation

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -583,8 +583,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             //   - bao-console is the serial debug console handler
             //   - [planned] pddb server
             //   - [planned] vault application
-            let bao_rram_pkgs = ["xous-ticktimer", "xous-log", "xous-names", "usb-cramium"].to_vec();
-            let bao_swap_pkgs = ["cram-hal-service", "bao-console"].to_vec(); /* "bao-video" */
+            let bao_rram_pkgs = ["xous-ticktimer", "xous-log", "xous-names" /* "usb-cramium" */].to_vec(); /* "usb-cramium" */
+            let bao_swap_pkgs = ["cram-hal-service", "bao-console", "bao-video"].to_vec(); /* "bao-video" */
             if !builder.is_swap_set() {
                 builder.set_swap(0, 8 * 1024 * 1024);
             }
@@ -598,8 +598,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             if false {
                 builder.add_loader_feature("userspace-swap-debug");
                 builder.add_feature("debug-print-swapper");
-            } else {
+                // use this to enable debug in USB, when the package is selected
                 builder.add_feature("debug-print-usb");
+            } else {
             }
             builder.add_kernel_feature("debug-swap");
             // builder.add_kernel_feature("debug-print");
