c2c1c8bc913624c55b22dbaefcd10e18276cd8d3
bunnie <bunnie@kosagi.com>
2025-02-11 16:24:51 +0800

    update top level libs and cargo for all the ported-in files

--- libs/ux-api/src/lib.rs
+++ libs/ux-api/src/lib.rs
@@ -1,4 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+mod blitstr2;
 pub mod minigfx;
 pub mod widgets;
+mod wordwrap;
+#[macro_use]
+mod style_macros;
+mod fontmap;
+pub mod platform;
+
+pub const SYSTEM_STYLE: crate::minigfx::GlyphStyle = crate::minigfx::GlyphStyle::Tall;
