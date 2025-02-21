07563c44db572a196631f5b3a15c7e39e4db584b
bunnie <bunnie@kosagi.com>
2025-02-21 17:43:23 +0800

    add precursor as a platform to ux-api

---
+++ libs/ux-api/src/platform/precursor.rs
@@ -0,0 +1,8 @@
+pub const LINES: isize = 536;
+pub const WIDTH: isize = 336;
+pub const HEIGHT: isize = LINES;
+
+pub const FB_WIDTH_WORDS: usize = 11;
+pub const FB_WIDTH_PIXELS: usize = WIDTH as usize;
+pub const FB_LINES: usize = LINES as usize;
+pub const FB_SIZE: usize = FB_WIDTH_WORDS * FB_LINES; // 44 bytes by 536 lines
