8acb9e7521302b6f141b3b6252bf3c480e91e3f9
bunnie <bunnie@kosagi.com>
2025-02-21 23:41:44 +0800

    add platform implementations of the frame buffer trait

--- libs/ux-api/src/platform/precursor.rs
+++ libs/ux-api/src/platform/precursor.rs
@@ -6,3 +6,6 @@ pub const FB_WIDTH_WORDS: usize = 11;
 pub const FB_WIDTH_PIXELS: usize = WIDTH as usize;
 pub const FB_LINES: usize = LINES as usize;
 pub const FB_SIZE: usize = FB_WIDTH_WORDS * FB_LINES; // 44 bytes by 536 lines
+
+// For passing frame buffer references
+pub type FbRaw = [u32];
