286bfb81351805750dd0da0f53f598a3fd94ee88
bunnie <bunnie@kosagi.com>
2025-02-28 17:52:20 +0800

    fix hosted mode build

--- services/graphics-server/src/backend/minifb.rs
+++ services/graphics-server/src/backend/minifb.rs
@@ -179,8 +179,8 @@ impl FrameBuffer for XousDisplay {
     ///
     /// Note: this has not be carefully tested as this API is not used by the legacy code base.
     /// Anyone using this API for the first time may benefit in checking that it is correct.
-    fn get_pixel(&mut self, p: Point) -> Option<ColorNative> {
-        let fb: &mut [u32] = &mut self.emulated_buffer;
+    fn get_pixel(&self, p: Point) -> Option<ColorNative> {
+        let fb: &[u32] = &self.emulated_buffer;
         let clip_y: usize;
         if p.y >= LCD_LINES {
             return None;
