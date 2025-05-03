9ca7b3822277a392ff37de2d6c0505cc88a68caf
bunnie <bunnie@kosagi.com>
2025-05-04 03:33:37 +0800

    add pddb into the hw config

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -607,8 +607,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             //   - [planned] pddb server
             //   - [planned] vault application
             let bao_rram_pkgs = ["xous-ticktimer", "xous-log", "xous-names" /* "usb-cramium" */].to_vec(); /* "usb-cramium" */
-            let bao_swap_pkgs =
-                ["cram-hal-service", "bao-console", "bao-video", "modals" /* "pddb" */].to_vec(); /* "bao-video" */
+            let bao_swap_pkgs = ["cram-hal-service", "bao-console", "bao-video", "modals", "pddb"].to_vec(); /* "bao-video" */
             if !builder.is_swap_set() {
                 builder.set_swap(0, 8 * 1024 * 1024);
             }
