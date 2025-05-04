71f444ff3d3e6af56682f80622935c5ce54b4065
bunnie <bunnie@kosagi.com>
2025-05-05 01:48:28 +0800

    API framework for the keystore

--- services/keystore/src/main.rs
+++ services/keystore/src/main.rs
@@ -1,1 +1,13 @@
+use keystore_api::*;
 
+fn main() -> ! {
+    log_server::init_wait().unwrap();
+    log::set_max_level(log::LevelFilter::Info);
+    log::info!("my PID is {}", xous::process::id());
+
+    let xns = xous_names::XousNames::new().unwrap();
+    // TODO: limit connections to this server?? once we know all that will connect
+    let keys_sid = xns.register_name(SERVER_NAME_KEYS, None).expect("can't register server");
+
+    loop {}
+}
