5e805196e607a6c73efc5a1d49a334cc593745b5
bunnie <bunnie@kosagi.com>
2025-11-14 19:14:25 +0800

    fix typo

--- bao1x-boot/boot1/src/secboot.rs
+++ bao1x-boot/boot1/src/secboot.rs
@@ -49,7 +49,7 @@ pub fn try_boot(or_die: bool) {
             if tag == *bao1x_api::pubkeys::KEYSLOT_INITIAL_TAGS[bao1x_api::pubkeys::DEVELOPER_KEY_SLOT]
                 || k == bao1x_api::pubkeys::DEVELOPER_KEY_SLOT
             {
-                crate::println!("Developer key detected, ensuring secret are erased");
+                crate::println!("Developer key detected, ensuring secrets are erased");
                 bao1x_hal::sigcheck::erase_secrets();
             }
             // this has to be called *after* erase_secrets, because we can't erase the secrets
