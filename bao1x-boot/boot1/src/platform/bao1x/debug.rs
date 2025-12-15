b509fffc51877fd7086cdc3f8f0716178b9fb575
bunnie <bunnie@kosagi.com>
2025-12-16 00:57:24 +0800

    cleanup perclk setting

--- bao1x-boot/boot1/src/platform/bao1x/debug.rs
+++ bao1x-boot/boot1/src/platform/bao1x/debug.rs
@@ -130,7 +130,7 @@ pub fn setup_console<T: IoSetup + IoGpio>(
     udma_global.map_event(uart_id, PeriphEventType::Uart(EventUartOffset::Tx), EventChannel::Channel1);
 
     let baudrate: u32 = crate::UART_BAUD;
-    let freq: u32 = perclk / 2;
+    let freq: u32 = perclk;
 
     // the address of the UART buffer is "hard-allocated" at an offset one page from the top of
     // IFRAM0. This is a convention that must be respected by the UDMA UART library implementation
