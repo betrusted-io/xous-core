32397a8d9f5025a5495f10657511c29f59241a25
bunnie <bunnie@kosagi.com>
2026-01-18 21:41:40 +0800

    upgrade tag message to beta-0

--- bao1x-boot/boot1/src/main.rs
+++ bao1x-boot/boot1/src/main.rs
@@ -33,7 +33,7 @@ use crate::secboot::try_boot;
 // Notes:
 // - "Towards" - not a release yet, but working towards the stated milestone
 // - Eliminating "Towards" is done at the tag-out point.
-const RELEASE_DESCRIPTION: &'static str = "Towards Beta-0";
+const RELEASE_DESCRIPTION: &'static str = "Beta-0";
 
 static UART_RX: Mutex<RefCell<VecDeque<u8>>> = Mutex::new(RefCell::new(VecDeque::new()));
 #[allow(dead_code)]
