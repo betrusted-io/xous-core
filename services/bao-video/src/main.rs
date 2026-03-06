017ab2a5fdb91c25919f786ae6b235b6a9d22948
bunnie <bunnie@kosagi.com>
2026-03-06 19:18:05 +0800

    report QR code when test code is scanned

--- services/bao-video/src/main.rs
+++ services/bao-video/src/main.rs
@@ -621,6 +621,14 @@ pub fn wrapped_main(main_thread_token: MainThreadToken) -> ! {
                                     // stop on the next frame.
                                     if let Some(mut envelope) = qr_request.take() {
                                         let metadata = format!("{:?}", meta);
+                                        if content.starts_with("test://") {
+                                            log::info!(
+                                                "{}{},{}",
+                                                bao1x_hal::board::BOOKEND_START,
+                                                content,
+                                                bao1x_hal::board::BOOKEND_END
+                                            );
+                                        }
                                         let acquisition =
                                             QrAcquisition { content: Some(content), meta: Some(metadata) };
                                         let mut response = unsafe {
