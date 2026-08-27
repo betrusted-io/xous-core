431e17f40acbb8e1e45c4d6be0885e33db4dc01d
bunnie <bunnie@kosagi.com>
2026-08-27 17:38:51 +0800

    fix broken CI build

--- bao1x-boot/boot1/src/repl.rs
+++ bao1x-boot/boot1/src/repl.rs
@@ -857,7 +857,7 @@ impl Repl {
                 // put random data in collateral - to simulate a third party keying
                 let slot_mgr = bao1x_hal::acram::SlotManager::new();
                 let mut rram = bao1x_hal::rram::Reram::new();
-                let slot = &bao1x_api::offsets::COLLATERAL;
+                let slot = &bao1x_api::offsets::COLLATERAL_ERASURE_ALIAS;
                 let mut trng = super::trng::ManagedTrng::new();
                 // only clear ACL if it isn't already cleared
                 if slot_mgr
